6bfb9d79c22242a5457ef52fde68da9ba354b0fa
Art Navsegda <artnavsegda@gmail.com>
2021-05-02 04:33:27 +0300

    Update main.dart

--- myflu/myflutapp/lib/main.dart
+++ myflu/myflutapp/lib/main.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
 
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print('MyButton was tapped!');
+      },
+      child: Container(
+        height: 50.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
+        ),
+        child: Center(
+          child: Text('Engage'),
+        ),
+      ),
+    );
+  }
+}
+
 void main() {
   runApp(
-    Center(
-      child: Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: MyButton(),
+        ),
       ),
     ),
   );
