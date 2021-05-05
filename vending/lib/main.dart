a882659961800f2ce433d1ee3aaa2c69d385de6d
Art Navsegda <artnavsegda@gmail.com>
2021-05-05 20:04:52 +0300

    splash

--- vending/lib/main.dart
+++ vending/lib/main.dart
@@ -42,7 +42,7 @@ class _LoginPageState extends State<LoginPage> {
             SizedBox(height: 80.0),
             Column(
               children: <Widget>[
-                Image.asset('assets/diamond.png'),
+                Image.asset('assets/splash.png'),
                 SizedBox(height: 16.0),
                 Text('SHRINE'),
               ],
