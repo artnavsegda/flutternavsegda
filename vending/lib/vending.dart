ecef3139e8a688f6349a567b3990d9aaa891d26e
Art Navsegda <artnavsegda@gmail.com>
2021-05-06 10:39:05 +0300

    Update vending.dart

--- vending/lib/vending.dart
+++ vending/lib/vending.dart
@@ -5,14 +5,13 @@ class VendingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: DefaultTabController(
-        length: 3,
+        length: 2,
         child: Scaffold(
           appBar: AppBar(
             bottom: TabBar(
               tabs: [
-                Tab(icon: Icon(Icons.directions_car)),
-                Tab(icon: Icon(Icons.directions_transit)),
-                Tab(icon: Icon(Icons.directions_bike)),
+                Tab(text: "Аппараты"),
+                Tab(text: "Склад"),
               ],
             ),
             title: Text('Your Name'),
@@ -21,7 +20,6 @@ class VendingPage extends StatelessWidget {
             children: [
               Icon(Icons.directions_car),
               Icon(Icons.directions_transit),
-              Icon(Icons.directions_bike),
             ],
           ),
         ),
