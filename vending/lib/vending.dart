3cc139348dde324b84b9d3a113a98738b5118a62
Art Navsegda <artnavsegda@gmail.com>
2021-05-06 10:26:49 +0300

    Tabs

--- vending/lib/vending.dart
+++ vending/lib/vending.dart
@@ -3,15 +3,26 @@ import 'package:flutter/material.dart';
 class VendingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("hello"),
-      ),
-      body: SafeArea(
-        child: Center(
-          child: Text(
-            'Hello, world!',
-            textDirection: TextDirection.ltr,
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            ),
+            title: Text('Your Name'),
+          ),
+          body: TabBarView(
+            children: [
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
           ),
         ),
       ),
