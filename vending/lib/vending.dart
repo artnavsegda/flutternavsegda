dbcac415a402dca8d9071367c9ae69f03bb19b51
Art Navsegda <artnavsegda@gmail.com>
2021-05-06 09:09:22 +0300

    Update vending.dart

--- vending/lib/vending.dart
+++ vending/lib/vending.dart
@@ -3,10 +3,17 @@ import 'package:flutter/material.dart';
 class VendingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("hello"),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Text(
+            'Hello, world!',
+            textDirection: TextDirection.ltr,
+          ),
+        ),
       ),
     );
   }
