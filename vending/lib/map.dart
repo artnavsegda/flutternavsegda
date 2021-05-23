83dc5ebdaa6b46a0b26a91a04fbd80b9778c888f
Art Navsegda <artnavsegda@gmail.com>
2021-05-23 23:28:12 +0300

    Update map.dart

--- vending/lib/map.dart
+++ vending/lib/map.dart
@@ -1,16 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+Future<Machines> fetchAlbum() async {}
+
 class MapPage extends StatefulWidget {
   @override
   _MapPageState createState() => _MapPageState();
+}
+
+class Machines {
+  String gUID;
+  String name;
+  String address;
+  double latitude;
+  double longitude;
+  String iBeaconUDID;
+  String mACAddress;
+  int serviceDate;
+
+  Machines(
+      {this.gUID,
+      this.name,
+      this.address,
+      this.latitude,
+      this.longitude,
+      this.iBeaconUDID,
+      this.mACAddress,
+      this.serviceDate});
+
+  Machines.fromJson(Map<String, dynamic> json) {
+    gUID = json['GUID'];
+    name = json['Name'];
+    address = json['Address'];
+    latitude = json['Latitude'];
+    longitude = json['Longitude'];
+    iBeaconUDID = json['IBeaconUDID'];
+    mACAddress = json['MACAddress'];
+    serviceDate = json['ServiceDate'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['GUID'] = this.gUID;
+    data['Name'] = this.name;
+    data['Address'] = this.address;
+    data['Latitude'] = this.latitude;
+    data['Longitude'] = this.longitude;
+    data['IBeaconUDID'] = this.iBeaconUDID;
+    data['MACAddress'] = this.mACAddress;
+    data['ServiceDate'] = this.serviceDate;
+    return data;
+  }
 }
 
 class _MapPageState extends State<MapPage> {
   GoogleMapController mapController;
 
   final LatLng _center = const LatLng(45.521563, -122.677433);
-  //Future<Data> futureData;
+  Future<Machines> futureMachines;
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -19,7 +66,7 @@ class _MapPageState extends State<MapPage> {
   @override
   void initState() {
     super.initState();
-    //futureData = fetchData();
+    futureMacines = fetchMacines();
   }
 
   @override
