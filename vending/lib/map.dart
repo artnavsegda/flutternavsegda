710b184332de39b1c1d4466a5f9e5e7278f07b47
Art Navsegda <artnavsegda@gmail.com>
2021-05-23 23:32:47 +0300

    Update map.dart

--- vending/lib/map.dart
+++ vending/lib/map.dart
@@ -1,7 +1,25 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-Future<Machines> fetchMachines() async {}
+Future<Machines> fetchMachines() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Machines.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load machines');
+  }
+}
 
 class MapPage extends StatefulWidget {
   @override
