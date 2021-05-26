1bc8748e107e1cdf8942de011478acc0a6f78f15
Art Navsegda <artnavsegda@gmail.com>
2021-05-26 21:15:05 +0300

    locations

---
+++ vending/lib/src/locations.dart
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2019 Google LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:json_annotation/json_annotation.dart';
+
+part 'locations.g.dart';
+
+@JsonSerializable()
+class Machine {
+  Machine(
+      {required this.GUID,
+      required this.Name,
+      required this.Address,
+      required this.Latitude,
+      required this.Longitude,
+      required this.IBeaconUDID,
+      required this.MACAddress,
+      required this.Start,
+      required this.Finish,
+      this.ServiceDate});
+
+  factory Machine.fromJson(Map<String, dynamic> json) =>
+      _$MachineFromJson(json);
+  Map<String, dynamic> toJson() => _$MachineToJson(this);
+
+  final String GUID;
+  final String Name;
+  final String Address;
+  final double Latitude;
+  final double Longitude;
+  final String IBeaconUDID;
+  final String MACAddress;
+  final String Start;
+  final String Finish;
+  final int? ServiceDate;
+}
+
+Future<List<Machine>> getMachines() async {
+  // Retrieve the locations of Google offices
+  final response = await http.get(
+      Uri.parse('https://app.tseh85.com/service/api/vending/machines'),
+      headers: {
+        'Token':
+            'I9AHcsqu+0q4LsfEyDPrk7giWL1B4TEVTXu4XWTZyGzEgneula0iinS4C6L7bds2',
+      });
+  if (response.statusCode == 200) {
+    Iterable l = json.decode(response.body);
+    return List<Machine>.from(l.map((model) => Machine.fromJson(model)));
+  } else {
+    throw HttpException(
+        'Unexpected status code ${response.statusCode}:'
+        ' ${response.reasonPhrase}',
+        uri: Uri.parse('https://app.tseh85.com/service/api/vending/machines'));
+  }
+}
---
+++ vending/lib/src/locations.g.dart
@@ -0,0 +1,35 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'locations.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Machine _$MachineFromJson(Map<String, dynamic> json) {
+  return Machine(
+    GUID: json['GUID'] as String,
+    Name: json['Name'] as String,
+    Address: json['Address'] as String,
+    Latitude: (json['Latitude'] as num).toDouble(),
+    Longitude: (json['Longitude'] as num).toDouble(),
+    IBeaconUDID: json['IBeaconUDID'] as String,
+    MACAddress: json['MACAddress'] as String,
+    Start: json['Start'] as String,
+    Finish: json['Finish'] as String,
+    ServiceDate: json['ServiceDate'] as int?,
+  );
+}
+
+Map<String, dynamic> _$MachineToJson(Machine instance) => <String, dynamic>{
+      'GUID': instance.GUID,
+      'Name': instance.Name,
+      'Address': instance.Address,
+      'Latitude': instance.Latitude,
+      'Longitude': instance.Longitude,
+      'IBeaconUDID': instance.IBeaconUDID,
+      'MACAddress': instance.MACAddress,
+      'Start': instance.Start,
+      'Finish': instance.Finish,
+      'ServiceDate': instance.ServiceDate,
+    };
