c45183532a5961014c1cabb354a09cbae2a303de
Art Navsegda <artnavsegda@gmail.com>
2021-05-26 08:15:03 +0300

    Create locations.dart

---
+++ demomap/lib/src/locations.dart
@@ -0,0 +1,75 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:json_annotation/json_annotation.dart';
+
+part 'locations.g.dart';
+
+@JsonSerializable()
+class LatLng {
+  LatLng({
+    required this.lat,
+    required this.lng,
+  });
+
+  factory LatLng.fromJson(Map<String, dynamic> json) => _$LatLngFromJson(json);
+  Map<String, dynamic> toJson() => _$LatLngToJson(this);
+
+  final double lat;
+  final double lng;
+}
+
+@JsonSerializable()
+class Machine {
+  Machine({
+    required this.address,
+    required this.id,
+    required this.image,
+    required this.lat,
+    required this.lng,
+    required this.name,
+    required this.phone,
+    required this.region,
+  });
+
+  factory Machine.fromJson(Map<String, dynamic> json) =>
+      _$MachineFromJson(json);
+  Map<String, dynamic> toJson() => _$MachineToJson(this);
+
+  final String address;
+  final String id;
+  final String image;
+  final double lat;
+  final double lng;
+  final String name;
+  final String phone;
+  final String region;
+}
+
+@JsonSerializable()
+class Locations {
+  Locations({
+    required this.machines,
+  });
+
+  factory Locations.fromJson(Map<String, dynamic> json) =>
+      _$LocationsFromJson(json);
+  Map<String, dynamic> toJson() => _$LocationsToJson(this);
+
+  final List<Machine> machines;
+}
+
+Future<Locations> getGooglemachines() async {
+  const googleLocationsURL = 'https://about.google/static/data/locations.json';
+
+  // Retrieve the locations of Google machines
+  final response = await http.get(Uri.parse(googleLocationsURL));
+  if (response.statusCode == 200) {
+    return Locations.fromJson(json.decode(response.body));
+  } else {
+    throw HttpException(
+        'Unexpected status code ${response.statusCode}:'
+        ' ${response.reasonPhrase}',
+        uri: Uri.parse(googleLocationsURL));
+  }
+}
