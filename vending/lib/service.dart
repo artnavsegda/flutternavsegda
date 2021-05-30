edf171c24f1f86071422a9a2b332595e94f6b824
Art Navsegda <artnavsegda@gmail.com>
2021-05-31 02:53:59 +0300

    Create service.dart

---
+++ vending/lib/service.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ServiceScreen extends StatefulWidget {
+  @override
+  _ServiceScreenState createState() => _ServiceScreenState();
+}
+
+class _ServiceScreenState extends State<ServiceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
