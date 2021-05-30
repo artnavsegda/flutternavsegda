721188b4fbaab401af5c43261da1366f5bf46144
Art Navsegda <artnavsegda@gmail.com>
2021-05-30 20:31:36 +0300

    Update storage.dart

--- vending/lib/storage.dart
+++ vending/lib/storage.dart
@@ -5,19 +5,19 @@ class StoragePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(children: [
       ListTile(
-        title: Text('Primary text'),
-        leading: Icon(Icons.directions_car),
+        title: Text('Получение'),
+        leading: Icon(Icons.add_shopping_cart),
         subtitle: Text('Metadata'),
       ),
       ListTile(
-        title: Text('Primary text'),
-        leading: Icon(Icons.directions_car),
-        subtitle: Text('Metadata'),
+        title: Text('Сдача'),
+        leading: Icon(Icons.remove_shopping_cart),
+        subtitle: Text('Сдача старого товара на склад'),
       ),
       ListTile(
-        title: Text('Primary text'),
-        leading: Icon(Icons.directions_car),
-        subtitle: Text('Metadata'),
+        title: Text('Сумка'),
+        leading: Icon(Icons.backpack),
+        subtitle: Text('Cписок товаров по аппаратам для загрузки'),
       ),
     ]);
   }
