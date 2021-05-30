4832a04395ed009f1af3d1ccd02e6aa4abcd2960
Art Navsegda <artnavsegda@gmail.com>
2021-05-30 20:22:57 +0300

    Update storage.dart

--- vending/lib/storage.dart
+++ vending/lib/storage.dart
@@ -4,6 +4,11 @@ class StoragePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(children: [
+      ListTile(
+        title: Text('Primary text'),
+        leading: Icon(Icons.label),
+        subtitle: Text('Metadata'),
+      ),
       Icon(Icons.directions_car),
     ]);
   }
