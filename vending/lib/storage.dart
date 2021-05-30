54f6a744ae392731b8aa9dc8cb050e7009d2eadc
Art Navsegda <artnavsegda@gmail.com>
2021-05-30 20:51:01 +0300

    Update storage.dart

--- vending/lib/storage.dart
+++ vending/lib/storage.dart
@@ -7,8 +7,16 @@ class StoragePage extends StatelessWidget {
       ListTile(
         title: Text('Получение'),
         leading: Icon(Icons.add_shopping_cart),
-        subtitle: Text('Metadata'),
-        onTap: () {},
+        subtitle: Text('Получение товара по накладной'),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute<void>(
+              builder: (BuildContext context) => FullScreenDialog(),
+              fullscreenDialog: true,
+            ),
+          );
+        },
       ),
       ListTile(
         title: Text('Сдача'),
@@ -23,5 +31,20 @@ class StoragePage extends StatelessWidget {
         onTap: () {},
       ),
     ]);
+  }
+}
+
+class FullScreenDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF6200EE),
+        title: Text('Full-screen Dialog'),
+      ),
+      body: Center(
+        child: Text("Full-screen dialog"),
+      ),
+    );
   }
 }
