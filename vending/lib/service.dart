34dce303a0e26e18cb2bd24c07cccae92c24629c
Art Navsegda <artnavsegda@gmail.com>
2021-05-31 08:18:56 +0300

    Update service.dart

--- vending/lib/service.dart
+++ vending/lib/service.dart
@@ -1,6 +1,45 @@
 import 'package:flutter/material.dart';
 import 'src/products.dart';
 
+class ProductItem extends StatefulWidget {
+  const ProductItem({
+    Key? key,
+    required Product product,
+  })  : _product = product,
+        super(key: key);
+
+  final Product _product;
+
+  @override
+  _ProductItemState createState() => _ProductItemState();
+}
+
+class _ProductItemState extends State<ProductItem> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+/*     return Text(widget._product.Name); */
+    return ListTile(
+      title: Text(widget._product.Name),
+      trailing: Container(
+        width: 112,
+        child: Row(
+          children: <Widget>[
+            IconButton(
+                icon: new Icon(Icons.remove),
+                onPressed: () => setState(() => _counter--)),
+            Text(_counter.toString()),
+            IconButton(
+                icon: new Icon(Icons.add),
+                onPressed: () => setState(() => _counter++))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class ServiceScreen extends StatefulWidget {
   const ServiceScreen({
     Key? key,
@@ -22,7 +61,8 @@ class _ServiceScreenState extends State<ServiceScreen> {
         body: ListView.builder(
           itemCount: widget._products.length,
           itemBuilder: (context, index) {
-            return Text(widget._products[index].Name);
+/*             return Text(widget._products[index].Name); */
+            return ProductItem(product: widget._products[index]);
           },
         ));
   }
