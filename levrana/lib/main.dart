21be51f408eef00eabc9c6da9c9ed293f75c8550
Art Navsegda <artnavsegda@gmail.com>
2021-06-02 05:28:10 +0300

    Update main.dart

--- levrana/lib/main.dart
+++ levrana/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
-String getCatalog = """
+/* String getCatalog = """
 query getCatalog {
   getCatalog {
     iD
@@ -10,6 +10,15 @@ query getCatalog {
     childs {
       name
     }
+  }
+}
+"""; */
+
+const String getCatalog = """
+query getCatalog {
+  getCatalog {
+    iD
+    name
   }
 }
 """;
@@ -120,35 +129,37 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Query(
+        options: QueryOptions(
+          document:
+              gql(getCatalog), // this is the query string you just created
         ),
-      ),
+        // Just like in apollo refetch() could be used to manually trigger a refetch
+        // while fetchMore() can be used for pagination purpose
+        builder: (result, {fetchMore, refetch}) {
+          if (result.hasException) {
+            return Text(result.exception.toString());
+          }
+
+          if (result.isLoading) {
+            return Text('Loading');
+          }
+
+          // it can be either Map or List
+          print(result);
+          List catalog = result.data!['getCatalog'];
+
+          return ListView.builder(
+              itemCount: catalog.length,
+              itemBuilder: (context, index) {
+                final section = catalog[index];
+
+                return Text(section['name']);
+              });
+        },
+      )),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
