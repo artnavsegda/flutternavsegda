1dd736c4dfd87d9ec68810949149f7741432d0ce
Art Navsegda <artnavsegda@gmail.com>
2021-06-03 10:00:53 +0300

    Update main.dart

--- levrana/lib/main.dart
+++ levrana/lib/main.dart
@@ -66,39 +66,12 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
           primarySwatch: Colors.green,
         ),
-        home: MyHomePage(title: 'Flutter Demo Home Page'),
+        home: MyHomePage(title: "hello"),
       ),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class CatalogPage extends StatelessWidget {
@@ -157,29 +130,111 @@ class CatalogPage extends StatelessWidget {
   }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _catalogID = 0;
-  List _catalog = [];
-
+class MainPage extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        body: Navigator(
+          onGenerateRoute: (settings) => MaterialPageRoute<void>(
+              builder: (BuildContext context) => Scaffold(
+                    body: Center(
+                      child: TextButton(
+                        child: Text("Helloaa"),
+                        onPressed: () => Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => Scaffold(
+                              body: Center(
+                                child: Text("Hello"),
+                              ),
+                              appBar: AppBar(
+                                title: Text("Hello"),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    appBar: AppBar(
+                      title: Text("Hello"),
+                    ),
+                  ),
+              settings: settings),
+        ),
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                color: Color.fromRGBO(0, 0, 0, 0.07),
+                blurRadius: 100.0,
+                offset: Offset(0.0, -27),
+              ),
+              BoxShadow(
+                color: Color.fromRGBO(0, 0, 0, 0.0417275),
+                blurRadius: 22.3363,
+                offset: Offset(0.0, -6.0308),
+              ),
+              BoxShadow(
+                color: Color.fromRGBO(0, 0, 0, 0.0282725),
+                blurRadius: 6.6501,
+                offset: Offset(0.0, -1.79553),
+              )
+            ],
+          ),
+          child: BottomNavigationBar(
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Image(image: AssetImage('assets/ic-24/icon-24-user.png')),
+                label: 'User',
+              ),
+              BottomNavigationBarItem(
+                icon: Image(
+                    image: AssetImage('assets/ic-24/icon-24-catalog-v3.png')),
+                label: 'Business',
+              ),
+              BottomNavigationBarItem(
+                icon: Image(
+                    image: AssetImage('assets/ic-24/icon-24-shopping.png')),
+                label: 'School',
+              ),
+              BottomNavigationBarItem(
+                icon: Image(image: AssetImage('assets/ic-24/icon-24-user.png')),
+                label: 'School',
+              ),
+              BottomNavigationBarItem(
+                icon: Image(image: AssetImage('assets/ic-24/icon-24-user.png')),
+                label: 'School',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Query(
+      body: Query(
         options: QueryOptions(
           document:
               gql(getCatalog), // this is the query string you just created
         ),
-        // Just like in apollo refetch() could be used to manually trigger a refetch
-        // while fetchMore() can be used for pagination purpose
         builder: (result, {fetchMore, refetch}) {
           if (result.hasException) {
             return Text(result.exception.toString());
@@ -192,48 +247,13 @@ class _MyHomePageState extends State<MyHomePage> {
           // it can be either Map or List
           print(result.data!['getCatalog']);
           List catalog = result.data!['getCatalog'];
-          if (_catalog.length != 0) catalog = _catalog;
 
           return CatalogPage(
             catalog: catalog,
             title: "Каталог",
           );
-
-/*           return ListView.separated(
-            itemCount: catalog.length,
-            itemBuilder: (context, index) {
-              final section = catalog[index];
-
-              return ListTile(
-                dense: true,
-                title: Text(
-                  section['name'],
-                  style: GoogleFonts.montserrat(fontSize: 16),
-                ),
-                trailing: Icon(Icons.navigate_next),
-                onTap: () {
-                  print(section['iD']);
-                  setState(() {
-                    _catalog = section['childs'];
-                  });
-                },
-              );
-            },
-            separatorBuilder: (context, index) {
-              return Divider(
-                height: 1,
-                indent: 20,
-                endIndent: 20,
-              );
-            },
-          ); */
         },
-      )),
-/*       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),  */ // This trailing comma makes auto-formatting nicer for build methods.
+      ),
       bottomNavigationBar: Container(
         decoration: BoxDecoration(
           boxShadow: <BoxShadow>[
