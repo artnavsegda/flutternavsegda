330158e2758768f0de51f67b0bab8cbba3741a69
Art Navsegda <artnavsegda@gmail.com>
2021-06-03 06:52:27 +0300

    Update main.dart

--- navigation/lib/main.dart
+++ navigation/lib/main.dart
@@ -37,7 +37,22 @@ class MyApp extends StatelessWidget {
                   ),
               settings: settings),
         ),
-        bottomNavigationBar: Text("Hello"),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Business',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'School',
+            ),
+          ],
+        ),
       ),
     );
   }
