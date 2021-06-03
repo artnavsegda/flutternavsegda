c85162353f4e57a1a89c388dc6271ea66cfa7450
Art Navsegda <artnavsegda@gmail.com>
2021-06-03 06:49:15 +0300

    Update main.dart

--- navigation/lib/main.dart
+++ navigation/lib/main.dart
@@ -12,33 +12,31 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       home: Scaffold(
         body: Navigator(
-          onGenerateRoute: (settings) {
-            return MaterialPageRoute<void>(
-                builder: (BuildContext context) => Scaffold(
-                      body: Center(
-                        child: TextButton(
-                          child: Text("Helloaa"),
-                          onPressed: () {
-                            print("Click");
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => Scaffold(
-                                      body: Center(
-                                        child: Text("Hello"),
-                                      ),
-                                      appBar: AppBar(
-                                        title: Text("Hello"),
-                                      ),
-                                    )));
-                          },
+          onGenerateRoute: (settings) => MaterialPageRoute<void>(
+              builder: (BuildContext context) => Scaffold(
+                    body: Center(
+                      child: TextButton(
+                        child: Text("Helloaa"),
+                        onPressed: () => Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => Scaffold(
+                              body: Center(
+                                child: Text("Hello"),
+                              ),
+                              appBar: AppBar(
+                                title: Text("Hello"),
+                              ),
+                            ),
+                          ),
                         ),
                       ),
-                      appBar: AppBar(
-                        title: Text("Hello"),
-                      ),
                     ),
-                settings: settings);
-          },
-        ), //Center(child: Text("Hello")),
+                    appBar: AppBar(
+                      title: Text("Hello"),
+                    ),
+                  ),
+              settings: settings),
+        ),
         bottomNavigationBar: Text("Hello"),
       ),
     );
