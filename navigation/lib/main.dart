9fd452e34b10638e90e352d89d275b2b2182b0b3
Art Navsegda <artnavsegda@gmail.com>
2021-06-03 06:45:20 +0300

    Update main.dart

--- navigation/lib/main.dart
+++ navigation/lib/main.dart
@@ -10,30 +10,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-/*       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/': (BuildContext context) => const Scaffold(
-              body: Center(child: Text("Hello")),
-            ),
-      }, */
       home: Scaffold(
         body: Navigator(
           onGenerateRoute: (settings) {
             return MaterialPageRoute<void>(
                 builder: (BuildContext context) => Scaffold(
-                      body: Center(child: Text("Hello")),
+                      body: Center(
+                        child: TextButton(
+                          child: Text("Helloaa"),
+                          onPressed: () {
+                            print("Click");
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => Scaffold(
+                                      body: Center(
+                                        child: Text("Hello"),
+                                      ),
+                                      appBar: AppBar(
+                                        title: Text("Hello"),
+                                      ),
+                                    )));
+                          },
+                        ),
+                      ),
                       appBar: AppBar(
                         title: Text("Hello"),
                       ),
