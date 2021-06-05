2ef85b9568f769afad0fe5d5b39854dc5242d2ac
Art Navsegda <artnavsegda@gmail.com>
2021-06-05 05:23:21 +0300

    Update main.dart

--- vending/lib/main.dart
+++ vending/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
 import 'login.dart';
+import 'vending.dart';
 
 void main() {
   runApp(
@@ -18,7 +19,7 @@ class AppModel with ChangeNotifier {
   String token = "";
   String userName = "";
 
-  Future login(String login, String password) async {
+  Future<String> login(String login, String password) async {
     print('login $login pass $password');
     var url =
         Uri.parse('https://app.tseh85.com/service/api/AuthenticateVending');
@@ -41,7 +42,7 @@ class AppModel with ChangeNotifier {
     });
   }
 
-  Future startup() {
+  Future<String> startup() {
     return SharedPreferences.getInstance().then((prefs) {
       userName = prefs.getString('username') ?? "";
       token = prefs.getString('token') ?? "";
@@ -51,15 +52,27 @@ class AppModel with ChangeNotifier {
 }
 
 class VendingApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var appState = context.read<AppModel>();
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: LoginPage(title: 'Цехомат'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
+        home: FutureBuilder<String>(
+          future: appState.startup(),
+          builder: (buildContext, snapshot) {
+            if (snapshot.hasData) {
+              if (snapshot.data == "") {
+                return LoginPage(title: 'Цехомат');
+              } else {
+                return VendingPage();
+              }
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ));
   }
 }
