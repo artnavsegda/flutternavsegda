c84c22827536c8c78fc8800ae8f10475a6d37456
Art Navsegda <artnavsegda@gmail.com>
2021-06-08 14:35:39 +0300

    try this

--- levrana/lib/main.dart
+++ levrana/lib/main.dart
@@ -7,27 +7,7 @@ import 'dialog.dart';
 
 void main() async {
   await initHiveForFlutter();
-
-  final HttpLink httpLink = HttpLink(
-    'https://demo.cyberiasoft.com/levranaservice/graphql',
-  );
-
-  final AuthLink authLink = AuthLink(
-    getToken: () async =>
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ODAxMWM3Yi03MDE1LTRkNDAtYTUzYS04ZGIyMzc4YTJiMTMiLCJkZXZpY2VJZCI6ImhlbGxvIiwib1NUeXBlIjoiMSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkRldmljZSIsImV4cCI6MzMxNDc4MjkxODEsImlzcyI6IkxldnJhbmEiLCJhdWQiOiJDeWJlcmlhU29mdCJ9.S4DL2jgnDJYbUWwtfozOr9H2yQpuKEnYEvmnKbLqyC8',
-  );
-
-  final Link link = authLink.concat(httpLink);
-
-  ValueNotifier<GraphQLClient> client = ValueNotifier(
-    GraphQLClient(
-      link: link,
-      // The default store is the InMemoryStore, which does NOT persist to disk
-      cache: GraphQLCache(store: HiveStore()),
-    ),
-  );
-
-  runApp(MyApp(client: client));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -35,13 +15,28 @@ class MyApp extends StatelessWidget {
 
   const MyApp({
     Key? key,
-    required this.client,
   }) : super(key: key);
-
-  final ValueNotifier<GraphQLClient> client;
 
   @override
   Widget build(BuildContext context) {
+    final HttpLink httpLink = HttpLink(
+      'https://demo.cyberiasoft.com/levranaservice/graphql',
+    );
+
+    final AuthLink authLink = AuthLink(
+      getToken: () async =>
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ODAxMWM3Yi03MDE1LTRkNDAtYTUzYS04ZGIyMzc4YTJiMTMiLCJkZXZpY2VJZCI6ImhlbGxvIiwib1NUeXBlIjoiMSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkRldmljZSIsImV4cCI6MzMxNDc4MjkxODEsImlzcyI6IkxldnJhbmEiLCJhdWQiOiJDeWJlcmlhU29mdCJ9.S4DL2jgnDJYbUWwtfozOr9H2yQpuKEnYEvmnKbLqyC8',
+    );
+
+    final Link link = authLink.concat(httpLink);
+
+    ValueNotifier<GraphQLClient> client = ValueNotifier(
+      GraphQLClient(
+        link: link,
+        cache: GraphQLCache(store: HiveStore()),
+      ),
+    );
+
     return GraphQLProvider(
       client: client,
       child: MaterialApp(
