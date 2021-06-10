d27dd1f09f85ed0b5f4835edba4192beee8a1102
Art Navsegda <artnavsegda@gmail.com>
2021-06-10 20:29:19 +0300

    Update user.dart

--- levrana/lib/pages/user.dart
+++ levrana/lib/pages/user.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+const String getClientInfo = """
+query getClientInfo {
+  getClientInfo {
+    name,
+    phone,
+    name,
+    dateOfBirth,
+    gender,
+    eMail,
+    confirmedPhone,
+    confirmedEMail
+    isPassword,
+    points,
+    picture,
+    codeInviteFriend
+  }
+}
+""";
 
 class UserPage extends StatelessWidget {
   const UserPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Query(
+        options: QueryOptions(
+          document:
+              gql(getClientInfo), // this is the query string you just created
+        ),
+        builder: (result, {fetchMore, refetch}) {
+          return Column(
+            children: [
+              Text(result.data!['getClientInfo']['name']),
+            ],
+          );
+        });
   }
 }
