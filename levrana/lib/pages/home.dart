beb3b115360997b30e7eacbd7462e173bca246f9
Art Navsegda <artnavsegda@gmail.com>
2021-06-11 14:31:04 +0300

    Update home.dart

--- levrana/lib/pages/home.dart
+++ levrana/lib/pages/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
+import 'user.dart';
+
 const String getActions = """
 query getActions {
   getActions {
@@ -28,24 +30,34 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Query(
-        options: QueryOptions(
-          document:
-              gql(getActions), // this is the query string you just created
-        ),
-        builder: (result, {fetchMore, refetch}) {
-          print(result.data);
-          return Container(
-            height: 160,
-            child: PageView.builder(
-              itemCount: result.data!['getActions'].length,
-              itemBuilder: (context, index) {
-                //return Text(result.data!['getActions'][index]['name']);
-                return Image.network(
-                    result.data!['getActions'][index]['picture']);
-              },
-            ),
-          );
-        });
+    return Column(children: [
+      Query(
+          options: QueryOptions(
+            document:
+                gql(getClientInfo), // this is the query string you just created
+          ),
+          builder: (result, {fetchMore, refetch}) {
+            return Text(result.data!['getClientInfo']['points'].toString());
+          }),
+      Query(
+          options: QueryOptions(
+            document:
+                gql(getActions), // this is the query string you just created
+          ),
+          builder: (result, {fetchMore, refetch}) {
+            print(result.data);
+            return Container(
+              height: 160,
+              child: PageView.builder(
+                itemCount: result.data!['getActions'].length,
+                itemBuilder: (context, index) {
+                  //return Text(result.data!['getActions'][index]['name']);
+                  return Image.network(
+                      result.data!['getActions'][index]['picture']);
+                },
+              ),
+            );
+          }),
+    ]);
   }
 }
