ce938220e1372325433e8506094696f60ec70649
Art Navsegda <artnavsegda@gmail.com>
2021-06-15 11:03:41 +0300

    background

--- levrana/lib/pages/home.dart
+++ levrana/lib/pages/home.dart
@@ -38,101 +38,105 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                decoration: InputDecoration(
-                    isDense: true,
-                    contentPadding: EdgeInsets.all(10),
-                    border: new OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: const BorderRadius.all(
-                        const Radius.circular(12.0),
+          child: Stack(
+        children: [
+          Image(image: AssetImage('assets/background.png')),
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      isDense: true,
+                      contentPadding: EdgeInsets.all(10),
+                      border: new OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: const BorderRadius.all(
+                          const Radius.circular(12.0),
+                        ),
                       ),
-                    ),
-                    filled: true),
+                      filled: true),
+                ),
               ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
-                      colors: <Color>[Color(0xff76B36D), Color(0xffCCED89)])),
-              child: Query(
-                  options: QueryOptions(document: gql(getClientInfo)),
+              Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        colors: <Color>[Color(0xff76B36D), Color(0xffCCED89)])),
+                child: Query(
+                    options: QueryOptions(document: gql(getClientInfo)),
+                    builder: (result, {fetchMore, refetch}) {
+                      print(result.data);
+                      return Text(
+                          result.data!['getClientInfo']['points'].toString(),
+                          style: GoogleFonts.montserrat(
+                              fontSize: 40,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white));
+                    }),
+              ),
+              Query(
+                  options: QueryOptions(document: gql(getActions)),
                   builder: (result, {fetchMore, refetch}) {
-                    print(result.data);
-                    return Text(
-                        result.data!['getClientInfo']['points'].toString(),
-                        style: GoogleFonts.montserrat(
-                            fontSize: 40,
-                            fontWeight: FontWeight.w700,
-                            color: Colors.white));
+                    //print(result.data);
+                    return Container(
+                      height: 160,
+                      child: PageView.builder(
+                        controller: _controller,
+                        itemCount: result.data!['getActions'].length,
+                        itemBuilder: (context, index) {
+                          //return Text(result.data!['getActions'][index]['name']);
+                          return Padding(
+                            padding: const EdgeInsets.only(right: 10.0),
+                            child: Image.network(
+                                result.data!['getActions'][index]['picture']),
+                          );
+                        },
+                      ),
+                    );
                   }),
-            ),
-            Query(
-                options: QueryOptions(document: gql(getActions)),
-                builder: (result, {fetchMore, refetch}) {
-                  //print(result.data);
-                  return Container(
-                    height: 160,
-                    child: PageView.builder(
-                      controller: _controller,
-                      itemCount: result.data!['getActions'].length,
-                      itemBuilder: (context, index) {
-                        //return Text(result.data!['getActions'][index]['name']);
-                        return Padding(
-                          padding: const EdgeInsets.only(right: 10.0),
-                          child: Image.network(
-                              result.data!['getActions'][index]['picture']),
-                        );
-                      },
-                    ),
-                  );
-                }),
-            Query(
-                options: QueryOptions(document: gql(getTopBlocks)),
-                builder: (result, {fetchMore, refetch}) {
-                  //print(result.data);
-                  return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: result.data!['getTopBlocks']
-                          .map(
-                            (section) => Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(section['name'],
-                                      style: GoogleFonts.montserrat(
-                                        fontSize: 28,
-                                        fontWeight: FontWeight.w700,
-                                      )),
-                                  Wrap(
-                                      children: section['products']
-                                          .map((product) => SizedBox(
-                                                width: 200.0,
-                                                height: 200.0,
-                                                child: GridTile(
-                                                  child: Image.network(
-                                                      product['picture']),
-                                                  footer: GridTileBar(
-                                                      title: Text(
-                                                          product['name'])),
-                                                ),
-                                              ))
-                                          .toList()
-                                          .cast<Widget>()),
-                                  Text("Something")
-                                ]),
-                          )
-                          .toList()
-                          .cast<Widget>());
-                })
-          ],
-        ),
-      ),
+              Query(
+                  options: QueryOptions(document: gql(getTopBlocks)),
+                  builder: (result, {fetchMore, refetch}) {
+                    //print(result.data);
+                    return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: result.data!['getTopBlocks']
+                            .map(
+                              (section) => Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(section['name'],
+                                        style: GoogleFonts.montserrat(
+                                          fontSize: 28,
+                                          fontWeight: FontWeight.w700,
+                                        )),
+                                    Wrap(
+                                        children: section['products']
+                                            .map((product) => SizedBox(
+                                                  width: 200.0,
+                                                  height: 200.0,
+                                                  child: GridTile(
+                                                    child: Image.network(
+                                                        product['picture']),
+                                                    footer: GridTileBar(
+                                                        title: Text(
+                                                            product['name'])),
+                                                  ),
+                                                ))
+                                            .toList()
+                                            .cast<Widget>()),
+                                    Text("Something")
+                                  ]),
+                            )
+                            .toList()
+                            .cast<Widget>());
+                  })
+            ],
+          ),
+        ],
+      )),
     );
   }
 }
