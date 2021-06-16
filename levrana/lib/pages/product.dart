13dfb4d4f7491cd9f94ee31ca110fffdc8de6b33
Art Navsegda <artnavsegda@gmail.com>
2021-06-16 15:48:24 +0300

    Update product.dart

--- levrana/lib/pages/product.dart
+++ levrana/lib/pages/product.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
-const String getProduct = """
-query getProduct {
-  getProduct(productID: 2)
+const String getProduct = r'''
+query getProduct($productID: Int!) {
+  getProduct(productID: $productID)
   {
     name
     stickerPictures
     comment
   }
 }
-""";
+''';
 
 class ProductPage extends StatelessWidget {
   const ProductPage({Key? key, this.id = 0}) : super(key: key);
@@ -20,7 +20,12 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Query(
-        options: QueryOptions(document: gql(getProduct)),
+        options: QueryOptions(
+          document: gql(getProduct),
+          variables: {
+            'productID': id,
+          },
+        ),
         builder: (result, {fetchMore, refetch}) {
           return Scaffold(
             appBar: AppBar(
