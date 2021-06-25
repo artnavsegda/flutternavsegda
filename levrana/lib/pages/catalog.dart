bfc3e77e9a901e6b73991c9a1db23c5e03f93335
Art Navsegda <artnavsegda@gmail.com>
2021-06-25 23:59:08 +0300

    Update catalog.dart

--- levrana/lib/pages/catalog.dart
+++ levrana/lib/pages/catalog.dart
@@ -29,7 +29,7 @@ query getCatalog {
 
 const String getProducts = r'''
 query getProducts($catalogID: Int!, $cursor: String) {
-  getProducts(catalogID: $catalogID, first: 2, after: $cursor)
+  getProducts(catalogID: $catalogID, first: 1, after: $cursor)
   {
     totalCount
     pageInfo {
