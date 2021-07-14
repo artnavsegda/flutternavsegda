302b4058dc62d72f233c2fc987cdfa8ee06ca5a5
Art Navsegda <artnavsegda@gmail.com>
2021-07-15 02:32:27 +0500

    Update home.dart

--- levrana/lib/pages/home.dart
+++ levrana/lib/pages/home.dart
@@ -75,8 +75,6 @@ class HomePage extends StatelessWidget {
                 },
                 child: Container(
                   margin: EdgeInsets.fromLTRB(16, 21, 16, 0),
-                  width: 288,
-                  height: 164,
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.all(Radius.circular(16)),
                       gradient: LinearGradient(
@@ -86,62 +84,66 @@ class HomePage extends StatelessWidget {
                             Color(0xff76B36D),
                             Color(0xffCCED89)
                           ])),
-                  child: Stack(children: [
-                    Positioned(
-                        top: 20,
-                        left: 20,
-                        child: Image(
-                            image: AssetImage('assets/logotype-levrana.png'))),
-                    Positioned(
-                      left: 16,
-                      bottom: 10,
-                      child: Row(
-                        children: [
-                          Query(
-                              options:
-                                  QueryOptions(document: gql(getClientInfo)),
-                              builder: (result, {fetchMore, refetch}) {
-                                print(result.data);
+                  child: AspectRatio(
+                    aspectRatio: 16 / 9,
+                    child: Stack(children: [
+                      Positioned(
+                          top: 20,
+                          left: 20,
+                          child: Image(
+                              image:
+                                  AssetImage('assets/logotype-levrana.png'))),
+                      Positioned(
+                        left: 16,
+                        bottom: 10,
+                        child: Row(
+                          children: [
+                            Query(
+                                options:
+                                    QueryOptions(document: gql(getClientInfo)),
+                                builder: (result, {fetchMore, refetch}) {
+                                  print(result.data);
 
-                                if (result.hasException) {
-                                  return Text("0",
+                                  if (result.hasException) {
+                                    return Text("0",
+                                        style: GoogleFonts.montserrat(
+                                            fontSize: 40,
+                                            fontWeight: FontWeight.w700,
+                                            color: Colors.white));
+                                    return Text(result.exception.toString());
+                                  }
+
+                                  if (result.isLoading) {
+                                    return Center(
+                                      child: CircularProgressIndicator(),
+                                    );
+                                  }
+
+                                  return Text(
+                                      result.data!['getClientInfo']['points']
+                                          .toString(),
                                       style: GoogleFonts.montserrat(
                                           fontSize: 40,
                                           fontWeight: FontWeight.w700,
                                           color: Colors.white));
-                                  return Text(result.exception.toString());
-                                }
-
-                                if (result.isLoading) {
-                                  return Center(
-                                    child: CircularProgressIndicator(),
-                                  );
-                                }
-
-                                return Text(
-                                    result.data!['getClientInfo']['points']
-                                        .toString(),
-                                    style: GoogleFonts.montserrat(
-                                        fontSize: 40,
-                                        fontWeight: FontWeight.w700,
-                                        color: Colors.white));
-                              }),
-                          Image(
-                              image:
-                                  AssetImage('assets/ic-24/icon-24-bonus.png'))
-                        ],
+                                }),
+                            Image(
+                                image: AssetImage(
+                                    'assets/ic-24/icon-24-bonus.png'))
+                          ],
+                        ),
                       ),
-                    ),
-                    Positioned(
-                        top: 22,
-                        right: 16,
-                        child: Image(image: AssetImage('assets/Union.png'))),
-                    Positioned(
-                        right: 16,
-                        bottom: 37,
-                        child:
-                            Image(image: AssetImage('assets/Group 145.png'))),
-                  ]),
+                      Positioned(
+                          top: 22,
+                          right: 16,
+                          child: Image(image: AssetImage('assets/Union.png'))),
+                      Positioned(
+                          right: 16,
+                          bottom: 37,
+                          child:
+                              Image(image: AssetImage('assets/Group 145.png'))),
+                    ]),
+                  ),
                 ),
               ),
               Query(
