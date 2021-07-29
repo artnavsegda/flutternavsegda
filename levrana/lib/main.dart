21ce792bb852e5e69940e96d27236eaaa96aa3df
Art Navsegda <artnavsegda@gmail.com>
2021-07-29 20:14:28 +0300

    tf

--- levrana/lib/main.dart
+++ levrana/lib/main.dart
@@ -60,8 +60,14 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
             scaffoldBackgroundColor: Colors.white,
             primarySwatch: Colors.green,
-            textTheme: GoogleFonts.montserratTextTheme(
+            fontFamily: 'Montserrat',
+/*             textTheme: GoogleFonts.montserratTextTheme(
               Theme.of(context).textTheme,
+            ), */
+            textTheme: const TextTheme(
+              headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+              headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+              bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Montserrat'),
             ),
             elevatedButtonTheme: ElevatedButtonThemeData(
                 style: ButtonStyle(
