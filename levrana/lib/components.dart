1d98eecaf18af4e2e37a4bf1c418d22113f8ed90
Art Navsegda <artnavsegda@gmail.com>
2021-08-03 16:02:03 +0300

    Create components.dart

---
+++ levrana/lib/components.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LevranaCheckbox extends StatelessWidget {
+  const LevranaCheckbox(
+      {Key? key, required this.value, required this.onChanged})
+      : super(key: key);
+
+  final bool? value;
+  final ValueChanged<bool?>? onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
