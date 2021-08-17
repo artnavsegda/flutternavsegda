a62162af52cee86b08c6d8e17abce4fc071adcf0
Art Navsegda <artnavsegda@gmail.com>
2021-08-17 11:33:12 +0300

    asd

--- levrana/lib/pages/catalog.dart
+++ levrana/lib/pages/catalog.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:levrana/pages/product.dart';
+import 'configurator.dart';
 
 const String getCatalog = r'''
 query getCatalog {
@@ -363,24 +364,6 @@ class FiltersPage extends StatelessWidget {
             Text("Hello"),
           ],
         ));
-  }
-}
-
-class Configurator extends StatelessWidget {
-  const Configurator({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Конфигуратор"),
-      ),
-      body: Center(
-        child: Text("Hello"),
-      ),
-    );
   }
 }
 
---
+++ levrana/lib/pages/configurator.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const String getConfigurator = r'''
+query getConfigurator
+{
+  getConfigurator {
+    iD
+    type
+    name
+    description
+    values {
+      iD
+      name
+      picture
+    }
+  }
+}
+''';
+
+class Configurator extends StatelessWidget {
+  const Configurator({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Конфигуратор"),
+      ),
+      body: Center(
+        child: Text("Hello"),
+      ),
+    );
+  }
+}
