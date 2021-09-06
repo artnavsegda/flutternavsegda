54ec69b499c2a34000ffb1e893096ede43a64f40
Art Navsegda <artnavsegda@gmail.com>
2021-09-06 19:34:57 +0300

    Update product.dart

--- levrana/lib/pages/product.dart
+++ levrana/lib/pages/product.dart
@@ -120,8 +120,15 @@ class _CharacteristicsElementState extends State<CharacteristicsElement> {
   @override
   Widget build(BuildContext context) {
     switch (widget.element['type']) {
-      //case 'TEXT':
-      //  return Text(element['name']);
+      case 'TEXT':
+        return Row(
+          children: [
+            Text(widget.element['name']),
+            Text(widget.element['values']
+                .map((element) => element['value'])
+                .reduce((value, element) => value + ',' + element))
+          ],
+        );
       case 'VOLUME':
       case 'SIZE':
         return Row(children: [
