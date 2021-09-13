f37c103f50c75af5c76b9385539fa493aab53739
Art Navsegda <artnavsegda@gmail.com>
2021-09-14 00:01:52 +0300

    Update product.dart

--- levrana/lib/pages/product.dart
+++ levrana/lib/pages/product.dart
@@ -271,94 +271,95 @@ class _ReviewPageState extends State<ReviewPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: MediaQuery.of(context).viewInsets,
-        child: Wrap(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Написать отзыв", style: TextStyle(fontSize: 32.0)),
-                  SizedBox(
-                    height: 24,
-                  ),
-                  Text("Ваша оценка",
-                      style: TextStyle(fontSize: 16.0, color: Colors.grey)),
-                  RatingBar(
-                    itemSize: 27,
-                    initialRating: rating / 2.0,
-                    direction: Axis.horizontal,
-                    allowHalfRating: true,
-                    itemCount: 5,
-                    ratingWidget: RatingWidget(
-                      full: Image.asset('assets/star.png'),
-                      half: Image.asset('assets/star_half.png'),
-                      empty: Image.asset('assets/star_border.png'),
+    return SafeArea(
+      child: Padding(
+          padding: MediaQuery.of(context).viewInsets,
+          child: Wrap(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Написать отзыв", style: TextStyle(fontSize: 32.0)),
+                    SizedBox(
+                      height: 24,
                     ),
-                    itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-                    onRatingUpdate: (newRating) {
-                      setState(() {
-                        rating = (newRating * 2).round();
-                      });
-                      print(newRating);
-                    },
-                  ),
-                  SizedBox(
-                    height: 18,
-                  ),
-                  TextField(
-                    decoration: const InputDecoration(
-                        border: UnderlineInputBorder(), labelText: 'Отзыв'),
-                    controller: myController,
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                  ),
-                ],
+                    Text("Ваша оценка",
+                        style: TextStyle(fontSize: 16.0, color: Colors.grey)),
+                    RatingBar(
+                      itemSize: 27,
+                      initialRating: rating / 2.0,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemCount: 5,
+                      ratingWidget: RatingWidget(
+                        full: Image.asset('assets/star.png'),
+                        half: Image.asset('assets/star_half.png'),
+                        empty: Image.asset('assets/star_border.png'),
+                      ),
+                      itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                      onRatingUpdate: (newRating) {
+                        setState(() {
+                          rating = (newRating * 2).round();
+                        });
+                        print(newRating);
+                      },
+                    ),
+                    SizedBox(
+                      height: 18,
+                    ),
+                    TextField(
+                      decoration: const InputDecoration(
+                          border: UnderlineInputBorder(), labelText: 'Отзыв'),
+                      controller: myController,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Row(
-                children: [
-                  Mutation(
-                      options: MutationOptions(
-                        document: gql(addReviewProduct),
-                        onError: (error) {
-                          print(error);
-                        },
-                        onCompleted: (dynamic resultData) async {
-                          print(resultData);
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  children: [
+                    Mutation(
+                        options: MutationOptions(
+                          document: gql(addReviewProduct),
+                          onError: (error) {
+                            print(error);
+                          },
+                          onCompleted: (dynamic resultData) async {
+                            print(resultData);
+                            Navigator.pop(context);
+                          },
+                        ),
+                        builder: (
+                          RunMutation runMutation,
+                          QueryResult? result,
+                        ) {
+                          return ElevatedButton(
+                              onPressed: () {
+                                runMutation({
+                                  'productID': widget.id,
+                                  'text': myController.text,
+                                  'mark': rating
+                                });
+                              },
+                              child: Text("ОТПРАВИТЬ"));
+                        }),
+                    SizedBox(width: 16),
+                    OutlinedButton(
+                        onPressed: () {
                           Navigator.pop(context);
                         },
-                      ),
-                      builder: (
-                        RunMutation runMutation,
-                        QueryResult? result,
-                      ) {
-                        return ElevatedButton(
-                            onPressed: () {
-                              runMutation({
-                                'productID': widget.id,
-                                'text': myController.text,
-                                'mark': rating
-                              });
-                            },
-                            child: Text("ОТПРАВИТЬ"));
-                      }),
-                  SizedBox(width: 16),
-                  OutlinedButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text("ОТМЕНА")),
-                ],
+                        child: Text("ОТМЕНА")),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 100),
-          ],
-        ));
+            ],
+          )),
+    );
   }
 }
 
