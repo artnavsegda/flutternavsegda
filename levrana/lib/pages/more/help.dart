bae04cad820dbf3d4b2585b21f55a5f1749ca77c
Art Navsegda <artnavsegda@gmail.com>
2021-10-08 16:30:48 +0300

    Update help.dart

--- levrana/lib/pages/more/help.dart
+++ levrana/lib/pages/more/help.dart
@@ -36,85 +36,86 @@ class HelpPage extends StatelessWidget {
                 iconRotationAngle: pi / 2,
                 expandIcon: Icons.chevron_right,
                 collapseIcon: Icons.chevron_right),
-            child: ListView(
-                children: result.data!['getFAQGroups']
-                    .map(
-                      (element) {
-                        return Padding(
-                          padding: const EdgeInsets.only(left: 8.0),
-                          child: ExpandablePanel(
-                              key: Key(element['iD'].toString()),
-                              header: Text(
-                                element['name'],
-                                style: TextStyle(
-                                    fontFamily: 'Montserrat',
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              collapsed: SizedBox.shrink(),
-                              expanded: Column(
-                                  children: element['questions']
-                                      .map((subElement) {
-                                        return ExpandableNotifier(
-                                          child: Padding(
-                                            padding: const EdgeInsets.only(
-                                                left: 8.0),
-                                            child: ExpandablePanel(
-                                                key: Key(subElement['iD']
-                                                    .toString()),
-                                                header: Padding(
-                                                  padding: const EdgeInsets
-                                                      .symmetric(vertical: 8.0),
-                                                  child: Text(
-                                                      subElement['question']),
-                                                ),
-                                                collapsed: SizedBox.shrink(),
-                                                expanded: Padding(
-                                                  padding:
-                                                      const EdgeInsets.only(
-                                                          right: 16,
-                                                          left: 24.0),
-                                                  child: Query(
-                                                      options: QueryOptions(
-                                                        document: gql(getFAQ),
-                                                        variables: {
-                                                          'fAQQuestionID':
-                                                              subElement['iD'],
-                                                        },
-                                                      ),
-                                                      builder: (result,
-                                                          {fetchMore,
-                                                          refetch}) {
-                                                        print(result);
-                                                        if (result
-                                                            .hasException) {
-                                                          return Text(result
-                                                              .exception
-                                                              .toString());
-                                                        }
-
-                                                        if (result.isLoading) {
-                                                          return Center(
-                                                            child:
-                                                                CircularProgressIndicator(),
-                                                          );
-                                                        }
-
+            child: ListView(children: [
+              SizedBox(
+                height: 24,
+              ),
+              ...result.data!['getFAQGroups']
+                  .map(
+                    (element) {
+                      return Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: ExpandablePanel(
+                            key: Key(element['iD'].toString()),
+                            header: Text(
+                              element['name'],
+                              style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            collapsed: SizedBox.shrink(),
+                            expanded: Column(
+                                children: element['questions']
+                                    .map((subElement) {
+                                      return ExpandableNotifier(
+                                        child: Padding(
+                                          padding:
+                                              const EdgeInsets.only(left: 8.0),
+                                          child: ExpandablePanel(
+                                              key: Key(
+                                                  subElement['iD'].toString()),
+                                              header: Padding(
+                                                padding:
+                                                    const EdgeInsets.symmetric(
+                                                        vertical: 8.0),
+                                                child: Text(
+                                                    subElement['question']),
+                                              ),
+                                              collapsed: SizedBox.shrink(),
+                                              expanded: Padding(
+                                                padding: const EdgeInsets.only(
+                                                    right: 16, left: 24.0),
+                                                child: Query(
+                                                    options: QueryOptions(
+                                                      document: gql(getFAQ),
+                                                      variables: {
+                                                        'fAQQuestionID':
+                                                            subElement['iD'],
+                                                      },
+                                                    ),
+                                                    builder: (result,
+                                                        {fetchMore, refetch}) {
+                                                      print(result);
+                                                      if (result.hasException) {
                                                         return Text(result
-                                                                .data!['getFAQ']
-                                                            ['answer']);
-                                                      }),
-                                                )),
-                                          ),
-                                        );
-                                      })
-                                      .toList()
-                                      .cast<Widget>())),
-                        );
-                      },
-                    )
-                    .toList()
-                    .cast<Widget>()),
+                                                            .exception
+                                                            .toString());
+                                                      }
+
+                                                      if (result.isLoading) {
+                                                        return Center(
+                                                          child:
+                                                              CircularProgressIndicator(),
+                                                        );
+                                                      }
+
+                                                      return Text(
+                                                          result.data!['getFAQ']
+                                                              ['answer']);
+                                                    }),
+                                              )),
+                                        ),
+                                      );
+                                    })
+                                    .toList()
+                                    .cast<Widget>())),
+                      );
+                    },
+                  )
+                  .toList()
+                  .cast<Widget>()
+            ]),
           );
         },
       ),
