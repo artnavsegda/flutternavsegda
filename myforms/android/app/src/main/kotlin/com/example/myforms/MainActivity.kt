76d5d445b2a90e3cb05b763179cef947ca522442
Art Navsegda <artnavsegda@gmail.com>
2021-10-08 06:27:44 +0300

    forms

---
+++ myforms/android/app/src/main/kotlin/com/example/myforms/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myforms
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ myforms/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_phone_field/form_builder_phone_field.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _formKey = GlobalKey<FormBuilderState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: FormBuilder(
+          key: _formKey,
+          child: Column(children: [
+            FormBuilderTextField(name: 'test'),
+            FormBuilderPhoneField(
+              name: 'phone_number',
+              decoration: const InputDecoration(
+                labelText: 'Phone Number',
+                hintText: 'Hint',
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
