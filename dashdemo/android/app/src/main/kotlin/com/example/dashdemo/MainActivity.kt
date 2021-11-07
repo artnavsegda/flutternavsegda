e3cb6000522cefcb4c5b9896c6f4ab0bc602e9c7
Art Navsegda <artnavsegda@gmail.com>
2021-11-08 00:10:03 +0300

    no chat demos

--- dashdemo/android/app/src/main/kotlin/com/example/dashdemo/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dashdemo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flyerdemo/android/app/src/main/kotlin/com/example/flyerdemo/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flyerdemo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
