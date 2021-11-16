71681fcdfef7e5b9ab72549c9e1bdc2332c2300c
Art Navsegda <artnavsegda@gmail.com>
2021-11-16 16:36:22 +0300

    Update main.dart

--- nord/lib/main.dart
+++ nord/lib/main.dart
@@ -35,8 +35,18 @@ class Onboarding extends StatelessWidget {
       ),
     );
 
-    return Scaffold(
-        body: Column(
+    return Scaffold(body: Welcome());
+  }
+}
+
+class Welcome extends StatelessWidget {
+  const Welcome({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -46,6 +56,57 @@ class Onboarding extends StatelessWidget {
 Мы движемся вперед, разработали удобную бонусную систему, обновили мобильное приложение, а заодно и сайт.
 Продолжайте писать эту красивую историю с нами.'''),
       ],
-    ));
+    );
+  }
+}
+
+class Push extends StatelessWidget {
+  const Push({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Уведомления'),
+        Text(
+            '''Хотите быть в курсе вкусных акций и предложений? Позвольте присылать уведомления.'''),
+      ],
+    );
+  }
+}
+
+class Location extends StatelessWidget {
+  const Location({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Местоположение'),
+        Text(
+            '''А теперь ―  геолокация. Разрешите доступ, это упростит поиск адреса для доставки или самовывоза Ваших любимых пирожных.'''),
+      ],
+    );
+  }
+}
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Все возможности'),
+        Text('''Войдите в аккаунт и используйте все возможности приложения.
+Копите баллы, заказывайте любимые блюда, узнавайте о новинках первыми.'''),
+      ],
+    );
   }
 }
