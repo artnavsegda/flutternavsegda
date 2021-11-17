33a48f1b2753052c5da6f4fdc9a740c607bdb649
Art Navsegda <artnavsegda@gmail.com>
2021-11-17 17:27:12 +0300

    main

--- nord/lib/main.dart
+++ nord/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'onboarding.dart';
+import 'pages/onboarding.dart';
 
 void main() {
   runApp(const MyApp());
---
+++ nord/lib/pages/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage>
+    with SingleTickerProviderStateMixin {
+  int _selectedIndex = 0;
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: 5);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TabBarView(
+        controller: _tabController,
+        children: [],
+      ),
+    );
+  }
+}
