54b5092d4ad5a17a22673e63cc2a9410a75c22e0
Art Navsegda <artnavsegda@gmail.com>
2021-11-17 16:34:52 +0300

    Update main.dart

--- nord/lib/main.dart
+++ nord/lib/main.dart
@@ -34,9 +34,7 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: nordTheme,
       home: const Onboarding(),
     );
   }
