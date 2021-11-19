8a0fb19314c7e209cda45b8a1b9c64959f2cab14
Art Navsegda <artnavsegda@gmail.com>
2021-11-19 19:41:28 +0300

    Update qr.dart

--- nord/lib/pages/home/qr.dart
+++ nord/lib/pages/home/qr.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class QrPage extends StatelessWidget {
   const QrPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Карта клиента'),
+      ),
+      body: Column(
+        children: [
+          QrImage(
+            data: "1234567890",
+            size: 200.0,
+          ),
+          Text('Предъявите на кассе'),
+          Text('+ 7 999 548 63 75'),
+          Text(
+              'Поднесите телефон к сканеру на кассе, чтобы начислить или списать бонусы'),
+          ElevatedButton(
+              onPressed: () {}, child: Text('Добавить карту в Google Pay')),
+        ],
+      ),
+    );
   }
 }
