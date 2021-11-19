c041f38fe5f058ebefa07666e812d23ded3b8d38
Art Navsegda <artnavsegda@gmail.com>
2021-11-19 19:10:01 +0300

    Update user.dart

--- nord/lib/pages/user/user.dart
+++ nord/lib/pages/user/user.dart
@@ -5,6 +5,39 @@ class UserPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView(
+      children: [
+        ListTile(
+          onTap: () {},
+          title: const Text("Princess Bean"),
+          trailing: Image.asset('assets/Icon Edit.png'),
+        ),
+        ListTile(
+          onTap: () {},
+          title: const Text("Подарки"),
+          leading: Image.asset('assets/Icon Present.png'),
+        ),
+        ListTile(
+          onTap: () {},
+          title: const Text("История заказов"),
+          leading: Image.asset('assets/Icon History.png'),
+        ),
+        ListTile(
+          onTap: () async {},
+          title: const Text("Адреса доставки"),
+          leading: Image.asset('assets/Icon Place.png'),
+        ),
+        ListTile(
+          onTap: () async {},
+          title: const Text("Смена пароля"),
+          leading: Image.asset('assets/Icon Lock.png'),
+        ),
+        ListTile(
+          onTap: () async {},
+          title: const Text("Выход из приложения"),
+          leading: Image.asset('assets/Icon Logout.png'),
+        ),
+      ],
+    );
   }
 }
