bd96eb8140023e8be4941c5ac0c08faf28beae13
Art Navsegda <artnavsegda@gmail.com>
2021-11-28 19:18:51 +0300

    Update home.dart

--- nord/lib/pages/home/home.dart
+++ nord/lib/pages/home/home.dart
@@ -9,11 +9,38 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: [
           ListTile(
+            onTap: () {
+              showModalBottomSheet(
+                isScrollControlled: true,
+                context: context,
+                builder: (context) {
+                  return SelectAddressBottomSheet();
+                },
+              );
+            },
             leading:
                 Image.asset('assets/Illustration Colored Delivery Options.png'),
             title: Text("Адрес доставки или кафе"),
           ),
           Text("Акции"),
+        ],
+      ),
+    );
+  }
+}
+
+class SelectAddressBottomSheet extends StatelessWidget {
+  const SelectAddressBottomSheet({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text('A'),
         ],
       ),
     );
