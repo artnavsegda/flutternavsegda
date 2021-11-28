a4b2729af265479eeea737b8b8dd29866a6f47a0
Art Navsegda <artnavsegda@gmail.com>
2021-11-28 19:59:11 +0300

    a

--- nord/lib/pages/shopping/shopping.dart
+++ nord/lib/pages/shopping/shopping.dart
@@ -5,6 +5,18 @@ class ShoppingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Image.asset('assets/Illustration Empty Cart.png'),
+        const Text('В корзине ничего нет...'),
+        const Text(
+            'Загляните в каталог — там всегда можно найти вкусные новинки или, если ищете что-то конкретное, воспользуйтесь поиском'),
+        ElevatedButton(
+            onPressed: () {
+              DefaultTabController.of(context)!.animateTo(1);
+            },
+            child: const Text('Перейти в каталог'))
+      ],
+    );
   }
 }
