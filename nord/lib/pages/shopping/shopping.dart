706052c24978e756563df168bacf50a079c48fba
Art Navsegda <artnavsegda@gmail.com>
2021-11-30 12:32:53 +0300

    Update shopping.dart

--- nord/lib/pages/shopping/shopping.dart
+++ nord/lib/pages/shopping/shopping.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../components/select_address_bottom_sheet.dart';
 import 'cart_is_empty.dart';
 
 class ShoppingPage extends StatelessWidget {
@@ -6,6 +7,30 @@ class ShoppingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Корзина'),
+        actions: [ElevatedButton(onPressed: () {}, child: Text('Очистить'))],
+      ),
+      body: Column(
+        children: [
+          ListTile(
+            onTap: () {
+              showModalBottomSheet(
+                isScrollControlled: true,
+                context: context,
+                builder: (context) {
+                  return const SelectAddressBottomSheet();
+                },
+              );
+            },
+            leading:
+                Image.asset('assets/Illustration Colored Delivery Options.png'),
+            title: const Text("Адрес доставки или кафе"),
+          ),
+        ],
+      ),
+    );
     return CartIsEmpty();
   }
 }
