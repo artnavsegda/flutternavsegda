4903e377bfa01ddc1eb43b5f4143ae74d38ce7ff
Art Navsegda <artnavsegda@gmail.com>
2021-12-13 14:40:59 +0300

    Update product.dart

--- nord/lib/pages/product/product.dart
+++ nord/lib/pages/product/product.dart
@@ -6,9 +6,17 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Печенье «Единорог в ро... '),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Печенье «Единорог в ро... '),
+        ),
+        body: Column(
+          children: [],
+        ),
+        bottomSheet: Row(
+          children: [
+            ElevatedButton(onPressed: () {}, child: Text('Добавить в корзину')),
+            TextButton(onPressed: () {}, child: Text('256'))
+          ],
+        ));
   }
 }
