591b2bacef24aedd0e5e4d2833300ecd4b488526
Art Navsegda <artnavsegda@gmail.com>
2021-12-20 00:06:37 +0300

    Update home.dart

--- nord/lib/pages/home/home.dart
+++ nord/lib/pages/home/home.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import '../../components/select_address_bottom_sheet.dart';
 import '../../components/product_card.dart';
@@ -28,58 +30,10 @@ class HomePage extends StatelessWidget {
             title: const Text("Адрес доставки или кафе"),
             trailing: Image.asset('assets/Icon-Expand-More.png'),
           ),
-          Container(
-            margin: const EdgeInsets.all(16.0),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(4),
-                gradient: const LinearGradient(
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                    colors: <Color>[Color(0xffCD0643), Color(0xffB0063A)])),
-            child: AspectRatio(
-              aspectRatio: 16 / 9,
-              child: Stack(
-                children: [
-                  Positioned(
-                    left: 16,
-                    top: 16,
-                    child: Image.asset('assets/Logo-Blue.png'),
-                  ),
-                  Positioned(
-                    left: 16,
-                    top: 40,
-                    child: Image.asset('assets/Logo-Red.png'),
-                  ),
-                  Positioned(
-                    right: 22,
-                    top: 22,
-                    child:
-                        Image.asset('assets/Special-Icon-QR-Code-Scanner.png'),
-                  ),
-                  Positioned(
-                    bottom: 8,
-                    left: 16,
-                    child: OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                      ),
-                      child: Text('Войти'),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const QrPage()));
-                      },
-                    ),
-                  ),
-                  Positioned(child: Image.asset('assets/Union.png'))
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: const Text("Акции",
+          const CardLoggedIn(),
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text("Акции",
                 style: TextStyle(fontFamily: 'Forum', fontSize: 24.0)),
           ),
           SizedBox(
@@ -87,45 +41,45 @@ class HomePage extends StatelessWidget {
             child: ListView(
               scrollDirection: Axis.horizontal,
               children: [
-                ActionCard(
+                const ActionCard(
                     actionName: 'Взрывная весна!\nОткрой новые вкусы сладко...',
                     actionImage: 'assets/placeholder/action/Illustration.png',
                     actionDate: '15 октября–27 ноября'),
-                ActionCard(
+                const ActionCard(
                     actionName: 'Шокодень',
                     actionImage: 'assets/placeholder/cake.png',
                     actionDate: 'Только до 31 октября'),
               ],
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: const Text("Новинки",
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text("Новинки",
                 style: TextStyle(fontFamily: 'Forum', fontSize: 24.0)),
           ),
           Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              ProductCard(
+              const ProductCard(
                 productImage: 'assets/placeholder/product1/Illustration.png',
                 productName: 'Торт «Сезонный» с ягодами',
                 productPrice: '420 ₽',
               ),
-              ProductCard(
+              const ProductCard(
                 productImage: 'assets/placeholder/product2/Illustration.png',
                 productName: 'Анна Павлова',
                 productPrice: '315 ₽',
               ),
             ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: const Text("Кондитерские и кафе",
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text("Кондитерские и кафе",
                 style: TextStyle(fontFamily: 'Forum', fontSize: 24.0)),
           ),
           Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
+            decoration: const BoxDecoration(
+              image: const DecorationImage(
                 image: AssetImage("assets/Map.png"),
                 fit: BoxFit.cover,
               ),
@@ -138,7 +92,7 @@ class HomePage extends StatelessWidget {
                   alignment: Alignment.bottomLeft,
                   child: ElevatedButton.icon(
                     label: const ImageIcon(AssetImage('assets/Icon-East.png')),
-                    icon: Text('Показать заведения на карте'),
+                    icon: const Text('Показать заведения на карте'),
                     onPressed: () {
                       Navigator.push(
                           context,
@@ -151,6 +105,172 @@ class HomePage extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class CardNotLoggedIn extends StatelessWidget {
+  const CardNotLoggedIn({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+          boxShadow: const [
+            BoxShadow(
+              color: Color(0x1F000000), //Color.fromRGBO(0, 0, 0, 0.12),
+              blurRadius: 20.0,
+              offset: Offset(0.0, 6.0),
+            ),
+          ],
+          borderRadius: BorderRadius.circular(4),
+          gradient: const LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: <Color>[Color(0xffCD0643), Color(0xffB0063A)])),
+      child: AspectRatio(
+        aspectRatio: 16 / 9,
+        child: Stack(
+          children: [
+            Positioned(
+              left: 16,
+              top: 16,
+              child: Image.asset('assets/Logo-Blue.png'),
+            ),
+            Positioned(
+              left: 16,
+              top: 40,
+              child: Image.asset('assets/Logo-Red.png'),
+            ),
+            Positioned(
+              right: 16,
+              top: 16,
+              child: Image.asset('assets/Special-Icon-QR-Code-Scanner.png'),
+            ),
+            Positioned(
+              bottom: 8,
+              left: 16,
+              child: OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                ),
+                child: const Text('Войти'),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const QrPage()));
+                },
+              ),
+            ),
+            Positioned(
+              bottom: 60,
+              left: 32,
+              child: Image.asset('assets/Union.png'),
+            ),
+            const Positioned(
+              bottom: 87,
+              left: 85,
+              child: const Text(
+                'Сюрпризы ждут вас!',
+                style: TextStyle(color: Colors.white),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardLoggedIn extends StatelessWidget {
+  const CardLoggedIn({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+          boxShadow: const [
+            BoxShadow(
+              color: Color(0x1F000000), //Color.fromRGBO(0, 0, 0, 0.12),
+              blurRadius: 20.0,
+              offset: Offset(0.0, 6.0),
+            ),
+          ],
+          borderRadius: BorderRadius.circular(4),
+          gradient: const LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: <Color>[Color(0xff0057B8), Color(0xff0A478B)])),
+      child: AspectRatio(
+        aspectRatio: 16 / 9,
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const QrPage()));
+            },
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 16,
+                  top: 16,
+                  child: Image.asset('assets/Logo-Blue.png'),
+                ),
+                Positioned(
+                  left: 16,
+                  top: 40,
+                  child: Image.asset('assets/Logo-Red.png'),
+                ),
+                Positioned(
+                  right: 16,
+                  top: 16,
+                  child: Image.asset('assets/Special-Icon-QR-Code-Scanner.png'),
+                ),
+                Positioned(
+                  top: 88,
+                  right: 43,
+                  child: Transform.rotate(
+                      angle: pi, child: Image.asset('assets/Union.png')),
+                ),
+                const Positioned(
+                  top: 113,
+                  right: 97,
+                  child: Text(
+                    'Ваша карта',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                const Positioned(
+                  bottom: 48,
+                  left: 16,
+                  child: const Text(
+                    'У вас',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                const Positioned(
+                  bottom: 16,
+                  left: 16,
+                  child: const Text(
+                    '120 бонусов',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Forum',
+                      fontSize: 24.0,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
