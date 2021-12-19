cc4ecac7c6791e65d940ebb8b6b26ff1ba827486
Art Navsegda <artnavsegda@gmail.com>
2021-12-19 21:49:42 +0300

    a

---
+++ nord/lib/pages/login/password.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PasswordPage extends StatelessWidget {
+  const PasswordPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ nord/lib/pages/login/sms.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SmsPage extends StatelessWidget {
+  const SmsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
