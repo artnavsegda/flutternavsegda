89b2f2c5ebab35cb8e51b8eebda5cd8b34db03d9
Art Navsegda <artnavsegda@gmail.com>
2021-12-19 04:34:48 +0300

    Update login.dart

--- nord/lib/pages/login/login.dart
+++ nord/lib/pages/login/login.dart
@@ -6,31 +6,43 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          Text('Войти или зарегистрироваться'),
-          Text('Мы отправим на номер SMS-сообщение с кодом потверждения'),
-          TextField(),
-          CheckboxListTile(
-            value: false,
-            onChanged: (newValue) {},
-            title: Text(
-                'Ознакомлен с условиями положения о защите персональных данных'),
-          ),
-          CheckboxListTile(
-            value: false,
-            onChanged: (newValue) {},
-            title: Text('Даю свое согласие на обработку персональных данных'),
-          ),
-          ElevatedButton(onPressed: () {}, child: Text('Далее')),
-          ListTile(
-            title: Text('Политика конфиденциальности'),
-          ),
-          ListTile(
-            title: Text('Правила бонусной программы'),
-          ),
-        ],
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Image.asset('assets/Icon-Close.png')),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Войти или\nзарегистрироваться',
+                style: TextStyle(fontFamily: 'Forum', fontSize: 24.0)),
+            SizedBox(height: 9),
+            Text('Мы отправим на номер SMS-сообщение с кодом потверждения'),
+            TextField(),
+            CheckboxListTile(
+              value: false,
+              onChanged: (newValue) {},
+              title: Text(
+                  'Ознакомлен с условиями положения о защите персональных данных'),
+            ),
+            CheckboxListTile(
+              value: false,
+              onChanged: (newValue) {},
+              title: Text('Даю свое согласие на обработку персональных данных'),
+            ),
+            ElevatedButton(onPressed: () {}, child: Text('Далее')),
+            ListTile(
+              title: Text('Политика конфиденциальности'),
+            ),
+            ListTile(
+              title: Text('Правила бонусной программы'),
+            ),
+          ],
+        ),
       ),
     );
   }
