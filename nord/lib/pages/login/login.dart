43771734b2cfa1adc5284e1a2cd949e2de7ee936
Art Navsegda <artnavsegda@gmail.com>
2021-12-24 05:31:43 +0300

    a

--- nord/lib/pages/login/login.dart
+++ nord/lib/pages/login/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../components/components.dart';
+import 'sms.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -14,44 +15,62 @@ class LoginPage extends StatelessWidget {
             },
             icon: Image.asset('assets/Icon-Close.png')),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          //crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Войти или\nзарегистрироваться',
-                style: TextStyle(fontFamily: 'Forum', fontSize: 24.0)),
-            SizedBox(height: 9),
-            Text('Мы отправим на номер SMS-сообщение с кодом потверждения'),
-            SizedBox(height: 24),
-            TextField(
-              decoration: const InputDecoration(
-                  labelText: "Номер телефона", hintText: '+7 (___) ___-__-__'),
+      body: ListView(
+        //crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text('Войти или\nзарегистрироваться',
+                    style: TextStyle(fontFamily: 'Forum', fontSize: 24.0)),
+                SizedBox(height: 9),
+                Text('Мы отправим на номер SMS-сообщение с кодом потверждения'),
+                SizedBox(height: 24),
+                TextField(
+                  decoration: const InputDecoration(
+                      labelText: "Номер телефона",
+                      hintText: '+7 (___) ___-__-__'),
+                ),
+                SizedBox(height: 32),
+                NordCheckboxTile(
+                    title: Text(
+                  'Ознакомлен с условиями положения о защите персональных данных',
+                  style: TextStyle(fontSize: 16),
+                )),
+                SizedBox(height: 32),
+                NordCheckboxTile(
+                    title: Text(
+                        'Даю свое согласие на обработку персональных данных',
+                        style: TextStyle(fontSize: 16))),
+                SizedBox(height: 32),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SmsPage()));
+                    },
+                    child: Text('Далее')),
+              ],
             ),
-            SizedBox(height: 32),
-            NordCheckboxTile(
-                title: Text(
-              'Ознакомлен с условиями положения о защите персональных данных',
-              style: TextStyle(fontSize: 16),
-            )),
-            SizedBox(height: 32),
-            NordCheckboxTile(
-                title: Text(
-                    'Даю свое согласие на обработку персональных данных',
-                    style: TextStyle(fontSize: 16))),
-            SizedBox(height: 32),
-            ElevatedButton(onPressed: () {}, child: Text('Далее')),
-            SizedBox(height: 32),
-            ListTile(
-              title: Text('Политика конфиденциальности'),
-              trailing: Image.asset('assets/Icon-Navigate-Next.png'),
+          ),
+          ListTile(
+            title: Text(
+              'Политика конфиденциальности',
+              style: TextStyle(color: Colors.red.shade900),
             ),
-            ListTile(
-              title: Text('Правила бонусной программы'),
-              trailing: Image.asset('assets/Icon-Navigate-Next.png'),
+            trailing: Image.asset('assets/Icon-Navigate-Next.png'),
+          ),
+          ListTile(
+            title: Text(
+              'Правила бонусной программы',
+              style: TextStyle(color: Colors.red.shade900),
             ),
-          ],
-        ),
+            trailing: Image.asset('assets/Icon-Navigate-Next.png'),
+          ),
+        ],
       ),
     );
   }
--- nord/lib/pages/login/sms.dart
+++ nord/lib/pages/login/sms.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import '../main.dart';
 
 class SmsPage extends StatelessWidget {
   const SmsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Image.asset('assets/Icon-West.png')),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Text('Введите\nкод подтверждения',
+                style: TextStyle(fontFamily: 'Forum', fontSize: 24.0)),
+            SizedBox(height: 9),
+            Text('Код подтверждения был отправлен на номер:\n+7 999 102-17-32'),
+            SizedBox(height: 24),
+            TextField(
+              decoration: const InputDecoration(labelText: "Код подтверждения"),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const MainPage()));
+                },
+                child: Text('Подтвердить')),
+            SizedBox(height: 8),
+            TextButton(onPressed: () {}, child: Text('Запросить новый код')),
+          ],
+        ),
+      ),
+    );
   }
 }
