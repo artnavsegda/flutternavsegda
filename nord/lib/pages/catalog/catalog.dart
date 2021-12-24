04f9d008e858596e87005c4931e8480ea977012a
Art Navsegda <artnavsegda@gmail.com>
2021-12-24 06:54:34 +0300

    Update catalog.dart

--- nord/lib/pages/catalog/catalog.dart
+++ nord/lib/pages/catalog/catalog.dart
@@ -13,59 +13,97 @@ class CatalogPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             AddressTile2(),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
+            Row(
+              children: [
+                Flexible(child: TextField()),
+                OutlinedButton(
+                  style: ButtonStyle(
+                      minimumSize:
+                          MaterialStateProperty.all(const Size(36.0, 36.0)),
+                      padding: MaterialStateProperty.all(EdgeInsets.all(0.0))),
+                  onPressed: () {},
+                  child: Image.asset('assets/Icon-Favorite-Outlined.png'),
+                ),
+              ],
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Flexible(child: TextField()),
-                      OutlinedButton(
-                        style: ButtonStyle(
-                            minimumSize: MaterialStateProperty.all(
-                                const Size(36.0, 36.0)),
-                            padding:
-                                MaterialStateProperty.all(EdgeInsets.all(0.0))),
-                        onPressed: () {},
-                        child: Image.asset('assets/Icon-Favorite-Outlined.png'),
+                      Text('Выпечка',
+                          style:
+                              TextStyle(fontFamily: 'Forum', fontSize: 24.0)),
+                      SizedBox(height: 12),
+                      Wrap(
+                        spacing: 8.0,
+                        runSpacing: 32.0,
+                        children: [
+                          ProductCard(
+                            productImage:
+                                'assets/placeholder/product1/Illustration.png',
+                            productName: 'Торт «Сезонный» с ягодами',
+                            productPrice: '420 ₽',
+                          ),
+                          ProductCard(
+                            productImage:
+                                'assets/placeholder/product2/Illustration.png',
+                            productName: 'Анна Павлова',
+                            productPrice: '315 ₽',
+                          ),
+                          ProductCard(
+                            productImage:
+                                'assets/placeholder/product1/Illustration.png',
+                            productName: 'Торт «Сезонный» с ягодами',
+                            productPrice: '420 ₽',
+                          ),
+                          ProductCard(
+                            productImage:
+                                'assets/placeholder/product2/Illustration.png',
+                            productName: 'Анна Павлова',
+                            productPrice: '315 ₽',
+                          ),
+                        ],
+                      ),
+                      Text('Выпечка',
+                          style:
+                              TextStyle(fontFamily: 'Forum', fontSize: 24.0)),
+                      SizedBox(height: 12),
+                      Wrap(
+                        spacing: 8.0,
+                        runSpacing: 32.0,
+                        children: [
+                          ProductCard(
+                            productImage:
+                                'assets/placeholder/product1/Illustration.png',
+                            productName: 'Торт «Сезонный» с ягодами',
+                            productPrice: '420 ₽',
+                          ),
+                          ProductCard(
+                            productImage:
+                                'assets/placeholder/product2/Illustration.png',
+                            productName: 'Анна Павлова',
+                            productPrice: '315 ₽',
+                          ),
+                          ProductCard(
+                            productImage:
+                                'assets/placeholder/product1/Illustration.png',
+                            productName: 'Торт «Сезонный» с ягодами',
+                            productPrice: '420 ₽',
+                          ),
+                          ProductCard(
+                            productImage:
+                                'assets/placeholder/product2/Illustration.png',
+                            productName: 'Анна Павлова',
+                            productPrice: '315 ₽',
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                  Text('Выпечка',
-                      style: TextStyle(fontFamily: 'Forum', fontSize: 24.0)),
-                  SizedBox(height: 12),
-                  Wrap(
-                    spacing: 8.0,
-                    runSpacing: 32.0,
-                    children: [
-                      ProductCard(
-                        productImage:
-                            'assets/placeholder/product1/Illustration.png',
-                        productName: 'Торт «Сезонный» с ягодами',
-                        productPrice: '420 ₽',
-                      ),
-                      ProductCard(
-                        productImage:
-                            'assets/placeholder/product2/Illustration.png',
-                        productName: 'Анна Павлова',
-                        productPrice: '315 ₽',
-                      ),
-                      ProductCard(
-                        productImage:
-                            'assets/placeholder/product1/Illustration.png',
-                        productName: 'Торт «Сезонный» с ягодами',
-                        productPrice: '420 ₽',
-                      ),
-                      ProductCard(
-                        productImage:
-                            'assets/placeholder/product2/Illustration.png',
-                        productName: 'Анна Павлова',
-                        productPrice: '315 ₽',
-                      ),
-                    ],
-                  ),
-                ],
+                ),
               ),
             ),
           ],
