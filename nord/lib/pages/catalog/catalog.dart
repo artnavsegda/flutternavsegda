ef7eee7afd0070133019c455eab8b5f26f852d79
Art Navsegda <artnavsegda@gmail.com>
2021-12-24 06:16:03 +0300

    Update catalog.dart

--- nord/lib/pages/catalog/catalog.dart
+++ nord/lib/pages/catalog/catalog.dart
@@ -21,9 +21,23 @@ class CatalogPage extends StatelessWidget {
                 children: [
                   Text('Выпечка',
                       style: TextStyle(fontFamily: 'Forum', fontSize: 24.0)),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  SizedBox(height: 12),
+                  Wrap(
+                    spacing: 8.0,
+                    runSpacing: 32.0,
                     children: [
+                      ProductCard(
+                        productImage:
+                            'assets/placeholder/product1/Illustration.png',
+                        productName: 'Торт «Сезонный» с ягодами',
+                        productPrice: '420 ₽',
+                      ),
+                      ProductCard(
+                        productImage:
+                            'assets/placeholder/product2/Illustration.png',
+                        productName: 'Анна Павлова',
+                        productPrice: '315 ₽',
+                      ),
                       ProductCard(
                         productImage:
                             'assets/placeholder/product1/Illustration.png',
