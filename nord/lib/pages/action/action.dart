11bf70dd6fd7c72cb4e594989f14ebff14164a7b
Art Navsegda <artnavsegda@gmail.com>
2021-12-27 13:16:19 +0300

    Update action.dart

--- nord/lib/pages/action/action.dart
+++ nord/lib/pages/action/action.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../components/product_card.dart';
 
 class ActionPage extends StatefulWidget {
   const ActionPage({Key? key}) : super(key: key);
@@ -26,7 +27,25 @@ class _ActionPageState extends State<ActionPage> {
               SizedBox(height: 50),
               Image.asset('assets/placeholder/action1/Illustration@3x.png'),
               Text(
-                  'Нашему организму просто необходимо определенное количество витаминов, чтобы чувствовать себя бодрыми, быть веселыми и не болеть. Сейчас самое подходящее время запастись ими перед долгой, холодно осенью.'),
+                'Нашему организму просто необходимо определенное количество витаминов, чтобы чувствовать себя бодрыми, быть веселыми и не болеть. Сейчас самое подходящее время запастись ими перед долгой, холодно осенью.',
+              ),
+              Text(
+                  'Успейте получить подарок, количество ограничено! Акция действует только в части наших кафе и кондитерских.'),
+              Text('Важно'),
+              Text(
+                  'Важно Сумма чека должна быть не менее 500 рублей без учёта бонусов. Приз выпадает за каждый чек на сумму более 500 рублей в акционный период. Подробнее об акции и условиях проведения читайте на сайте.'),
+              TextButton(onPressed: () {}, child: Text('Найти ближайшее кафе')),
+              Text('Товары, участвующие в акции'),
+              ProductCard(
+                productImage: 'assets/placeholder/product1/Illustration.png',
+                productName: 'Торт «Сезонный» с ягодами',
+                productPrice: '420 ₽',
+              ),
+              ProductCard(
+                productImage: 'assets/placeholder/product2/Illustration.png',
+                productName: 'Анна Павлова',
+                productPrice: '315 ₽',
+              ),
             ],
           ),
         ),
