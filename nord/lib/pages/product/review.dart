f206d5494e173fce5c1b6e3478bde113e787ed3f
Art Navsegda <artnavsegda@gmail.com>
2021-12-27 15:35:14 +0300

    rate bar

--- nord/lib/pages/product/review.dart
+++ nord/lib/pages/product/review.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class ReviewPage extends StatelessWidget {
   const ReviewPage({Key? key}) : super(key: key);
@@ -12,6 +13,22 @@ class ReviewPage extends StatelessWidget {
       body: Column(
         children: [
           Text('Оценка товара'),
+          RatingBar(
+            itemSize: 27,
+            initialRating: 4.0,
+            direction: Axis.horizontal,
+            allowHalfRating: false,
+            itemCount: 5,
+            ratingWidget: RatingWidget(
+              full: Image.asset('assets/Icon-Star-Rate.png'),
+              half: Image.asset('assets/Icon-Star-Rate-Outlined.png'),
+              empty: Image.asset('assets/Icon-Star-Rate-Outlined.png'),
+            ),
+            itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+            onRatingUpdate: (newRating) {
+              print(newRating);
+            },
+          ),
           TextField(),
           ElevatedButton(onPressed: () {}, child: Text('Оправить отзыв'))
         ],
