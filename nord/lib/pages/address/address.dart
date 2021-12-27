0a9a8be72cab8b4f92ef2fc9bdb1456e5fa2b6db
Art Navsegda <artnavsegda@gmail.com>
2021-12-27 16:04:22 +0300

    Update address.dart

--- nord/lib/pages/address/address.dart
+++ nord/lib/pages/address/address.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class AddressPage extends StatelessWidget {
   const AddressPage({Key? key}) : super(key: key);
@@ -9,10 +10,10 @@ class AddressPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Адрес доставки или кафе'),
       ),
-      body: Container(
-        child: ElevatedButton(
-          child: Text('Выбрать'),
-          onPressed: () {},
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(37.42796133580664, -122.085749655962),
+          zoom: 14.4746,
         ),
       ),
     );
