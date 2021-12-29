ca626f0dfe2f707947ffb1e6ea7cddb441c0b260
Art Navsegda <artnavsegda@gmail.com>
2021-12-30 01:22:58 +0300

    Update shop.dart

--- nord/lib/pages/shop/shop.dart
+++ nord/lib/pages/shop/shop.dart
@@ -6,24 +6,34 @@ class ShopPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Image.asset('assets/placeholder/shop.png'),
-          Positioned(
-            top: 36,
-            left: 12,
-            child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(32.0, 32.0),
-                  shape: CircleBorder(),
-                  primary: Colors.white54, // <-- Button color
-                  onPrimary: Colors.red, // <-- Splash color
-                ),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Image.asset('assets/Icon-Close.png')),
-          )
+          Stack(
+            children: [
+              Image.asset('assets/placeholder/shop.png'),
+              Positioned(
+                top: 36,
+                left: 12,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(32.0, 32.0),
+                      shape: CircleBorder(),
+                      primary: Colors.white54, // <-- Button color
+                      onPrimary: Colors.red, // <-- Splash color
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Image.asset('assets/Icon-Close.png')),
+              )
+            ],
+          ),
+          Text('Просвещения, 19 (ТК Норд)'),
+          Text('+7 (812) 611-09-27'),
+          Text('Завтра откроется в 10:00'),
+          Spacer(),
+          ElevatedButton(onPressed: () {}, child: Text('Построить маршрут'))
         ],
       ),
     );
