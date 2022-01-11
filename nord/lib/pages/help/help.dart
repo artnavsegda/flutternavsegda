3da2e38eac841754a738a97edbc532068377d4bc
Art Navsegda <artnavsegda@gmail.com>
2022-01-12 02:51:06 +0300

    Update help.dart

--- nord/lib/pages/help/help.dart
+++ nord/lib/pages/help/help.dart
@@ -9,6 +9,37 @@ class HelpPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Справка'),
       ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('Бонусная программа'),
+            trailing: Image.asset('assets/Icon-East.png'),
+          ),
+          ListTile(
+            title: Text('Доставка и оплата'),
+            trailing: Image.asset('assets/Icon-East.png'),
+          ),
+          ListTile(
+            title: Text('О компании'),
+            trailing: Image.asset('assets/Icon-East.png'),
+          ),
+          ListTile(
+            title: Text('Оптовые продажи'),
+            trailing: Image.asset('assets/Icon-East.png'),
+          ),
+          ListTile(
+            title: Text('О компании'),
+            trailing: Image.asset('assets/Icon-East.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+                '''Для улучшения работы приложения компания может использовать статистические и иные технические данные о действих пользователя при работе с приложением (перемещение между экранами, нажатие кнопок и т.д.). Персональная данные или любая финансовая информация не используется для данных целей.
+
+Основные положения сбора и хранения информации вы можете найти в Положении об Обработке и защите персональных данных.'''),
+          )
+        ],
+      ),
     );
   }
 }
