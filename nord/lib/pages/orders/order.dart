520559037532fdd291b21b3990b2f0a67b7ca4c2
Art Navsegda <artnavsegda@gmail.com>
2022-01-11 23:36:53 +0300

    Update order.dart

--- nord/lib/pages/orders/order.dart
+++ nord/lib/pages/orders/order.dart
@@ -7,7 +7,31 @@ class OrderPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      title: Text('Заказ №2564848'),
-    ));
+          title: Text('Заказ №2564848'),
+        ),
+        body: ListView(
+          children: [
+            Row(
+              children: [
+                Text('Оценка заказа'),
+              ],
+            ),
+            Text('Детали заказа'),
+            ListTile(
+              subtitle: Text('Доставка по адресу'),
+              title:
+                  Text('Санкт-Петербург, Дачный проспект, 36к3, квартира 410'),
+            ),
+            ListTile(
+              subtitle: Text('Дата и время доставки'),
+              title: Text('22 сентября, с 11:00 до 13:00'),
+            ),
+            ListTile(
+              subtitle: Text('Получатель'),
+              title: Text('Антон Горький, +7 (999) 102-17-32'),
+            ),
+            Text('Детали оплаты'),
+          ],
+        ));
   }
 }
