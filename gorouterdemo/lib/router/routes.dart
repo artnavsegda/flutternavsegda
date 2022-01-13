8b53da5793d751b0816293c0f35f19c415400ff3
Art Navsegda <artnavsegda@gmail.com>
2022-01-13 22:56:38 +0300

    go router demo

---
+++ gorouterdemo/lib/router/routes.dart
@@ -0,0 +1,206 @@
+/*
+ * Copyright (c) 2021 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../ui/create_account.dart';
+import '../ui/error_page.dart';
+import '../ui/home_screen.dart';
+import '../ui/more_info.dart';
+import '../ui/payment.dart';
+import '../ui/personal_info.dart';
+import '../ui/signin_info.dart';
+import '../constants.dart';
+import '../login_state.dart';
+import '../ui/login.dart';
+import '../ui/details.dart';
+
+class MyRouter {
+  final LoginState loginState;
+
+  MyRouter(this.loginState);
+
+  late final router = GoRouter(
+    refreshListenable: loginState,
+    debugLogDiagnostics: true,
+    urlPathStrategy: UrlPathStrategy.path,
+    routes: [
+      GoRoute(
+        name: rootRouteName,
+        path: '/',
+        redirect: (state) =>
+            state.namedLocation(homeRouteName, params: {'tab': 'shop'}),
+      ),
+      GoRoute(
+        name: loginRouteName,
+        path: '/login',
+        pageBuilder: (context, state) => MaterialPage<void>(
+          key: state.pageKey,
+          child: const Login(),
+        ),
+      ),
+      GoRoute(
+        name: createAccountRouteName,
+        path: '/create-account',
+        pageBuilder: (context, state) => MaterialPage<void>(
+          key: state.pageKey,
+          child: const CreateAccount(),
+        ),
+      ),
+      GoRoute(
+        name: homeRouteName,
+        path: '/home/:tab(shop|cart|profile)',
+        pageBuilder: (context, state) {
+          final tab = state.params['tab']!;
+          return MaterialPage<void>(
+            key: state.pageKey,
+            child: HomeScreen(tab: tab),
+          );
+        },
+        routes: [
+          GoRoute(
+            name: subDetailsRouteName,
+            path: 'details/:item',
+            pageBuilder: (context, state) => MaterialPage<void>(
+              key: state.pageKey,
+              child: Details(description: state.params['item']!),
+            ),
+          ),
+          GoRoute(
+            name: profilePersonalRouteName,
+            path: 'personal',
+            pageBuilder: (context, state) => MaterialPage<void>(
+              key: state.pageKey,
+              child: const PersonalInfo(),
+            ),
+          ),
+          GoRoute(
+            name: profilePaymentRouteName,
+            path: 'payment',
+            pageBuilder: (context, state) => MaterialPage<void>(
+              key: state.pageKey,
+              child: const Payment(),
+            ),
+          ),
+          GoRoute(
+            name: profileSigninInfoRouteName,
+            path: 'signin-info',
+            pageBuilder: (context, state) => MaterialPage<void>(
+              key: state.pageKey,
+              child: const SigninInfo(),
+            ),
+          ),
+          GoRoute(
+            name: profileMoreInfoRouteName,
+            path: 'more-info',
+            pageBuilder: (context, state) => MaterialPage<void>(
+              key: state.pageKey,
+              child: const MoreInfo(),
+            ),
+          ),
+        ],
+      ),
+      // forwarding routes to remove the need to put the 'tab' param in the code
+      GoRoute(
+        path: '/shop',
+        redirect: (state) =>
+            state.namedLocation(homeRouteName, params: {'tab': 'shop'}),
+      ),
+      GoRoute(
+        path: '/cart',
+        redirect: (state) =>
+            state.namedLocation(homeRouteName, params: {'tab': 'cart'}),
+      ),
+      GoRoute(
+        path: '/profile',
+        redirect: (state) =>
+            state.namedLocation(homeRouteName, params: {'tab': 'profile'}),
+      ),
+      GoRoute(
+        name: detailsRouteName,
+        path: '/details-redirector/:item',
+        redirect: (state) => state.namedLocation(
+          subDetailsRouteName,
+          params: {'tab': 'shop', 'item': state.params['item']!},
+        ),
+      ),
+      GoRoute(
+        name: personalRouteName,
+        path: '/profile-personal',
+        redirect: (state) => state.namedLocation(
+          profilePersonalRouteName,
+          params: {'tab': 'profile'},
+        ),
+      ),
+      GoRoute(
+        name: paymentRouteName,
+        path: '/profile-payment',
+        redirect: (state) => state.namedLocation(
+          profilePaymentRouteName,
+          params: {'tab': 'profile'},
+        ),
+      ),
+      GoRoute(
+        name: signinInfoRouteName,
+        path: '/profile-signin-info',
+        redirect: (state) => state.namedLocation(
+          profileSigninInfoRouteName,
+          params: {'tab': 'profile'},
+        ),
+      ),
+      GoRoute(
+        name: moreInfoRouteName,
+        path: '/profile-more-info',
+        redirect: (state) => state.namedLocation(
+          profileMoreInfoRouteName,
+          params: {'tab': 'profile'},
+        ),
+      ),
+    ],
+
+    errorPageBuilder: (context, state) => MaterialPage<void>(
+      key: state.pageKey,
+      child: ErrorPage(error: state.error),
+    ),
+
+    // redirect to the login page if the user is not logged in
+    redirect: (state) {
+      final loginLoc = state.namedLocation(loginRouteName);
+      final loggingIn = state.subloc == loginLoc;
+      final createAccountLoc = state.namedLocation(createAccountRouteName);
+      final creatingAccount = state.subloc == createAccountLoc;
+      final loggedIn = loginState.loggedIn;
+      final rootLoc = state.namedLocation(rootRouteName);
+
+      if (!loggedIn && !loggingIn && !creatingAccount) return loginLoc;
+      if (loggedIn && (loggingIn || creatingAccount)) return rootLoc;
+      return null;
+    },
+  );
+}
