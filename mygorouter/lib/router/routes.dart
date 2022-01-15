2bca2b4aef5a0af5840b6632c788dff2e0e36cca
Art Navsegda <artnavsegda@gmail.com>
2022-01-16 02:58:57 +0300

    login

--- mygorouter/lib/router/routes.dart
+++ mygorouter/lib/router/routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../login_state.dart';
+import '../pages/login.dart';
 
 class LevranaRouter {
   final LoginState loginState;
@@ -10,6 +11,22 @@ class LevranaRouter {
     refreshListenable: loginState,
     debugLogDiagnostics: true,
     urlPathStrategy: UrlPathStrategy.path,
-    routes: [],
+    routes: [
+      GoRoute(
+        name: 'root',
+        path: '/',
+        redirect: (state) =>
+            // TODO: Change to Home Route
+            state.namedLocation('login'),
+      ),
+      GoRoute(
+        name: 'login',
+        path: '/login',
+        pageBuilder: (context, state) => MaterialPage<void>(
+          key: state.pageKey,
+          child: const LoginPage(),
+        ),
+      ),
+    ],
   );
 }
