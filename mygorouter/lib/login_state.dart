ac043f2150c5fe608a031f1519617497c9ab3a03
Art Navsegda <artnavsegda@gmail.com>
2022-01-16 12:12:32 +0300

    Update login_state.dart

--- mygorouter/lib/login_state.dart
+++ mygorouter/lib/login_state.dart
@@ -4,15 +4,24 @@ import 'package:shared_preferences/shared_preferences.dart';
 class LoginState extends ChangeNotifier {
   final SharedPreferences prefs;
   bool _loggedIn = false;
+  String _token = '';
 
   LoginState(this.prefs) {
     loggedIn = prefs.getBool('LoggedIn') ?? false;
+    token = prefs.getString('token') ?? "";
   }
 
   bool get loggedIn => _loggedIn;
   set loggedIn(bool value) {
     _loggedIn = value;
     prefs.setBool('LoggedIn', value);
+    notifyListeners();
+  }
+
+  String get token => _token;
+  set token(String newToken) {
+    _token = newToken;
+    prefs.setString('token', newToken);
     notifyListeners();
   }
 
