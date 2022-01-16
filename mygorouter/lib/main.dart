df1d7086ded34304e56a9ceb1c3f18801575ff5f
Art Navsegda <artnavsegda@gmail.com>
2022-01-16 12:34:22 +0300

    ae

--- mygorouter/lib/main.dart
+++ mygorouter/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:provider/provider.dart';
 
 import 'login_state.dart';
 import 'router/routes.dart';
 
 Future<void> main() async {
+  await initHiveForFlutter();
   WidgetsFlutterBinding.ensureInitialized();
   final state = LoginState(await SharedPreferences.getInstance());
   state.checkLoggedIn();
@@ -30,17 +32,29 @@ class LevranaApp extends StatelessWidget {
           create: (BuildContext createContext) => LevranaRouter(loginState),
         ),
       ],
-      child: Builder(
-        builder: (BuildContext context) {
+      child: Consumer<LoginState>(
+        builder: (BuildContext context, model, child) {
           final router =
               Provider.of<LevranaRouter>(context, listen: false).router;
-          return MaterialApp.router(
-            routeInformationParser: router.routeInformationParser,
-            routerDelegate: router.routerDelegate,
-            debugShowCheckedModeBanner: false,
-            title: 'Levrana App',
-            theme: ThemeData(
-              primarySwatch: Colors.green,
+          return GraphQLProvider(
+            client: ValueNotifier(
+              GraphQLClient(
+                link: AuthLink(getToken: () => 'Bearer ' + model.token).concat(
+                  HttpLink(
+                    'https://demo.cyberiasoft.com/levranaservice/graphql',
+                  ),
+                ),
+                cache: GraphQLCache(store: HiveStore()),
+              ),
+            ),
+            child: MaterialApp.router(
+              routeInformationParser: router.routeInformationParser,
+              routerDelegate: router.routerDelegate,
+              debugShowCheckedModeBanner: false,
+              title: 'Levrana App',
+              theme: ThemeData(
+                primarySwatch: Colors.green,
+              ),
             ),
           );
         },
