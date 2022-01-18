b10b943aa6a3a4702bab5e21ba987491c6a13284
Art Navsegda <artnavsegda@gmail.com>
2022-01-18 03:54:07 +0300

    Update home.dart

--- mygorouter/lib/pages/home.dart
+++ mygorouter/lib/pages/home.dart
@@ -81,7 +81,7 @@ class HomePage extends StatelessWidget {
         return Text('more');
       case 'main':
       default:
-        return Text('main');
+        return MainPage();
     }
   }
 
@@ -136,6 +136,144 @@ class HomePage extends StatelessWidget {
           }
         },
       ),
+    );
+  }
+}
+
+class GraphProductAttribute {
+  GraphProductAttribute({
+    required this.iD,
+    required this.name,
+    required this.color,
+  });
+  int iD;
+  String name;
+  String color;
+
+  GraphProductAttribute.fromJson(Map<String, dynamic> json)
+      : iD = json['iD'],
+        name = json['name'],
+        color = json['color'];
+}
+
+class GraphProduct {
+  GraphProduct({
+    required this.iD,
+    this.type,
+    required this.familyID,
+    required this.topCatalogID,
+    required this.name,
+    this.picture,
+    required this.isFavorite,
+    required this.favorites,
+    required this.stickerPictures,
+    required this.attributes,
+  });
+  int iD;
+  String? type;
+  int familyID;
+  int topCatalogID;
+  String name;
+  String? picture;
+  bool isFavorite;
+  int favorites;
+  List<String> stickerPictures;
+  List<GraphProductAttribute> attributes;
+
+  GraphProduct.fromJson(Map<String, dynamic> json)
+      : iD = json['iD'],
+        type = json['type'],
+        familyID = json['familyID'],
+        topCatalogID = json['topCatalogID'],
+        name = json['name'],
+        picture = json['picture'],
+        isFavorite = json['isFavorite'],
+        favorites = json['favorites'],
+        stickerPictures = List<String>.from(json['stickerPictures']),
+        attributes = List<GraphProductAttribute>.from(json['attributes']
+            .map((model) => GraphProductAttribute.fromJson(model)));
+}
+
+class GraphTopBlock {
+  GraphTopBlock({
+    required this.iD,
+    required this.name,
+    required this.products,
+  });
+  int iD;
+  String name;
+  List<GraphProduct> products;
+
+  GraphTopBlock.fromJson(Map<String, dynamic> json)
+      : iD = json['iD'],
+        name = json['name'],
+        products = List<GraphProduct>.from(
+            json['products'].map((model) => GraphProduct.fromJson(model)));
+}
+
+const String getTopBlocks = r'''
+query getTopBlocks {
+  getTopBlocks
+  {
+    iD
+    name
+    products {
+      iD
+      type
+      familyID
+      topCatalogID
+      name
+      picture
+      isFavorite
+      favorites
+      stickerPictures
+      attributes {
+        iD
+        name
+        color
+      }
+    }
+  }
+}
+''';
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Query(
+      options: QueryOptions(
+        document: gql(getTopBlocks),
+      ),
+      builder: (result, {fetchMore, refetch}) {
+        if (result.isLoading || result.hasException) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        List<GraphTopBlock> topBlocks = List<GraphTopBlock>.from(result
+            .data!['getTopBlocks']
+            .map((model) => GraphTopBlock.fromJson(model)));
+
+        return ListView.builder(
+            itemCount: topBlocks.length,
+            itemBuilder: (context, index) {
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(topBlocks[index].name),
+                  Column(
+                      children: topBlocks[index]
+                          .products
+                          .map((e) => ListTile(title: Text(e.name)))
+                          .toList()
+                          .cast<Widget>()),
+                ],
+              );
+            });
+      },
     );
   }
 }
