a025713c1bea03ad9f4f30f2a78270d1bb5f728d
Art Navsegda <artnavsegda@gmail.com>
2022-01-30 02:41:59 +0300

    Update edit_user.dart

--- nord/lib/pages/user/edit_user.dart
+++ nord/lib/pages/user/edit_user.dart
@@ -14,89 +14,97 @@ class EditUser extends StatelessWidget {
             },
             icon: Image.asset('assets/Icon-West.png')),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Align(
-              alignment: Alignment.centerLeft,
-              child: InkWell(
-                onTap: () {
-                  showModalBottomSheet(
-                    backgroundColor: Colors.white,
-                    isScrollControlled: true,
-                    context: context,
-                    builder: (context) {
-                      return Column(
-                        mainAxisSize: MainAxisSize.min,
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: InkWell(
+                      onTap: () {
+                        showModalBottomSheet(
+                          backgroundColor: Colors.white,
+                          isScrollControlled: true,
+                          context: context,
+                          builder: (context) {
+                            return Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                ListTile(
+                                  title: const Center(
+                                      child: Text(
+                                    'Изменение фотографии',
+                                  )),
+                                  trailing:
+                                      Image.asset('assets/Icon-Close.png'),
+                                ),
+                                ListTile(
+                                  leading: Image.asset(
+                                      'assets/Icon-Photo-Camers.png'),
+                                  title: const Text('Запустить камеру'),
+                                ),
+                                ListTile(
+                                  leading: Image.asset('assets/Icon-List.png'),
+                                  title: const Text('Выбрать из галереи'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                      child: Stack(
+                        alignment: Alignment.center,
                         children: [
-                          ListTile(
-                            title: const Center(
-                                child: Text(
-                              'Изменение фотографии',
-                            )),
-                            trailing: Image.asset('assets/Icon-Close.png'),
+                          const CircleAvatar(
+                            radius: 48,
+                            foregroundImage: AssetImage('assets/treska.jpg'),
                           ),
-                          ListTile(
-                            leading:
-                                Image.asset('assets/Icon-Photo-Camers.png'),
-                            title: const Text('Запустить камеру'),
-                          ),
-                          ListTile(
-                            leading: Image.asset('assets/Icon-List.png'),
-                            title: const Text('Выбрать из галереи'),
+                          Image.asset(
+                            'assets/Icon-Photo-Camers.png',
+                            color: Colors.white,
                           ),
                         ],
-                      );
-                    },
-                  );
-                },
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    const CircleAvatar(
-                      radius: 48,
-                      foregroundImage: AssetImage('assets/treska.jpg'),
+                      ),
                     ),
-                    Image.asset(
-                      'assets/Icon-Photo-Camers.png',
-                      color: Colors.white,
-                    ),
-                  ],
-                ),
+                  ),
+                  const SizedBox(height: 24),
+                  const Text(
+                    "Личная информация",
+                    style: TextStyle(fontFamily: 'Forum', fontSize: 24),
+                  ),
+                  const SizedBox(height: 24),
+                  const TextField(
+                    decoration: InputDecoration(labelText: "Имя"),
+                  ),
+                  const SizedBox(height: 16),
+                  const TextField(
+                    decoration: InputDecoration(labelText: "Номер телефона"),
+                  ),
+                  const SizedBox(height: 16),
+                  const TextField(
+                    decoration: InputDecoration(labelText: "Email"),
+                  ),
+                  const SizedBox(height: 16),
+                  const TextField(
+                    decoration: InputDecoration(labelText: "Номер телефона"),
+                  ),
+                  const SizedBox(height: 16),
+                  const TextField(
+                    decoration: InputDecoration(labelText: "Номер телефона"),
+                  ),
+                  const SizedBox(height: 24),
+                  const Text(
+                    "Другие настройки",
+                    style: TextStyle(fontFamily: 'Forum', fontSize: 24),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 24),
-            const Text(
-              "Личная информация",
-              style: TextStyle(fontFamily: 'Forum', fontSize: 24),
-            ),
-            const SizedBox(height: 24),
-            const TextField(
-              decoration: InputDecoration(labelText: "Имя"),
-            ),
-            const SizedBox(height: 16),
-            const TextField(
-              decoration: InputDecoration(labelText: "Номер телефона"),
-            ),
-            const SizedBox(height: 16),
-            const TextField(
-              decoration: InputDecoration(labelText: "Email"),
-            ),
-            const SizedBox(height: 16),
-            const TextField(
-              decoration: InputDecoration(labelText: "Номер телефона"),
-            ),
-            const SizedBox(height: 16),
-            const TextField(
-              decoration: InputDecoration(labelText: "Номер телефона"),
-            ),
-            const SizedBox(height: 24),
-            const Text(
-              "Другие настройки",
-              style: TextStyle(fontFamily: 'Forum', fontSize: 24),
-            ),
-            const SizedBox(height: 16),
             SwitchListTile(
               title: const Text('Отказаться от бумажных чеков'),
               value: false,
@@ -107,12 +115,14 @@ class EditUser extends StatelessWidget {
               value: false,
               onChanged: (newVal) {},
             ),
-            const SizedBox(height: 24),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text("Сохранить"))
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Сохранить")),
+            )
           ],
         ),
       ),
