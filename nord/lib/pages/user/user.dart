9995667433fea22531b043dcaa376519e687199a
Art Navsegda <artnavsegda@gmail.com>
2022-01-29 22:29:23 +0300

    Update user.dart

--- nord/lib/pages/user/user.dart
+++ nord/lib/pages/user/user.dart
@@ -16,139 +16,164 @@ class UserPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        ListTile(
-          leading: const CircleAvatar(
-            foregroundImage: AssetImage('assets/treska.jpg'),
-          ),
-          onTap: () {
-            context.push('/editProfile');
-          },
-          title: const Text("Princess\nBean"),
-          trailing: Icon(SeverMetropol.Icon_Edit, color: Colors.red.shade900),
-        ),
-        AspectRatio(
-          aspectRatio: 16 / 9,
-          child: Container(
-              margin: const EdgeInsets.all(16.0),
-              padding: const EdgeInsets.all(16.0),
-              decoration: BoxDecoration(
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Color(0x1F000000), //Color.fromRGBO(0, 0, 0, 0.12),
-                      blurRadius: 20.0,
-                      offset: Offset(0.0, 6.0),
-                    ),
-                  ],
-                  borderRadius: BorderRadius.circular(4),
-                  gradient: const LinearGradient(
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
-                      colors: <Color>[Color(0xffCD0643), Color(0xffB0063A)])),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'У вас',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  const Text(
-                    '120 бонусов',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Forum',
-                      fontSize: 24.0,
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      OutlinedButton(
-                          style: OutlinedButton.styleFrom(
-                            backgroundColor: Colors.white,
-                          ),
-                          onPressed: () {},
-                          child: const Text('Позвать друга')),
-                      TextButton(
-                          style: TextButton.styleFrom(
-                            primary: Colors.white,
-                          ),
-                          onPressed: () {
-                            showModalBottomSheet(
-                              isScrollControlled: true,
-                              context: context,
-                              builder: (context) {
-                                return const GiftBonusModalSheet();
-                              },
-                            );
-                          },
-                          child: const Text('Подарить бонусы'))
-                    ],
-                  )
-                ],
-              )),
-        ),
-        ListTile(
-          onTap: () {},
-          title: const Text("Подарки"),
-          leading: Icon(SeverMetropol.Icon_Present, color: Colors.red.shade900),
-        ),
-        ListTile(
-          onTap: () {},
-          title: const Text("Промокод"),
-          leading:
-              Icon(SeverMetropol.Icon_Redeem_Card, color: Colors.red.shade900),
-        ),
-        ListTile(
-          onTap: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const OrdersPage()));
-          },
-          title: const Text("История заказов"),
-          leading: Icon(SeverMetropol.Icon_History, color: Colors.red.shade900),
-        ),
-        ListTile(
-          onTap: () async {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => const DeliveryAddressPage()));
-          },
-          title: const Text("Адреса доставки"),
-          leading: Icon(SeverMetropol.Icon_Place, color: Colors.red.shade900),
-        ),
-        const Divider(
-          color: Color(0xFFEFF3F4),
-          thickness: 2,
-          indent: 16,
-          endIndent: 16,
-        ),
-        ListTile(
-          onTap: () async {},
-          title: const Text("Смена пароля"),
-          leading: Icon(SeverMetropol.Icon_Lock, color: Colors.red.shade900),
-        ),
-        Mutation(
-          options: MutationOptions(
-            document: gql(logoffClient),
-            onCompleted: (result) {
-              Provider.of<LoginState>(context, listen: false).loggedIn = false;
-              context.push('/login');
-            },
-          ),
-          builder: (runMutation, result) {
-            return ListTile(
-              onTap: () async {
-                runMutation({});
-              },
-              title: const Text("Выход из приложения"),
-              leading:
-                  Icon(SeverMetropol.Icon_Logout, color: Colors.red.shade900),
+    return Query(
+        options: QueryOptions(document: gql(getClientInfo)),
+        builder: (result, {fetchMore, refetch}) {
+          if (result.isLoading && result.data == null) {
+            return const Center(
+              child: CircularProgressIndicator(),
             );
-          },
-        ),
-      ],
-    );
+          }
+
+          GraphClientFullInfo userInfo =
+              GraphClientFullInfo.fromJson(result.data!['getClientInfo']);
+
+          return ListView(
+            children: [
+              ListTile(
+                leading: const CircleAvatar(
+                  foregroundImage: AssetImage('assets/treska.jpg'),
+                ),
+                onTap: () {
+                  context.push('/editProfile');
+                },
+                title: Text(userInfo.name ?? "Незнакомец"),
+                trailing:
+                    Icon(SeverMetropol.Icon_Edit, color: Colors.red.shade900),
+              ),
+              AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Container(
+                    margin: const EdgeInsets.all(16.0),
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Color(
+                                0x1F000000), //Color.fromRGBO(0, 0, 0, 0.12),
+                            blurRadius: 20.0,
+                            offset: Offset(0.0, 6.0),
+                          ),
+                        ],
+                        borderRadius: BorderRadius.circular(4),
+                        gradient: const LinearGradient(
+                            begin: Alignment.centerLeft,
+                            end: Alignment.centerRight,
+                            colors: <Color>[
+                              Color(0xffCD0643),
+                              Color(0xffB0063A)
+                            ])),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'У вас',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Text(
+                          userInfo.points.toString() + ' бонусов',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Forum',
+                            fontSize: 24.0,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            OutlinedButton(
+                                style: OutlinedButton.styleFrom(
+                                  backgroundColor: Colors.white,
+                                ),
+                                onPressed: () {},
+                                child: const Text('Позвать друга')),
+                            TextButton(
+                                style: TextButton.styleFrom(
+                                  primary: Colors.white,
+                                ),
+                                onPressed: () {
+                                  showModalBottomSheet(
+                                    isScrollControlled: true,
+                                    context: context,
+                                    builder: (context) {
+                                      return const GiftBonusModalSheet();
+                                    },
+                                  );
+                                },
+                                child: const Text('Подарить бонусы'))
+                          ],
+                        )
+                      ],
+                    )),
+              ),
+              ListTile(
+                onTap: () {},
+                title: const Text("Подарки"),
+                leading: Icon(SeverMetropol.Icon_Present,
+                    color: Colors.red.shade900),
+              ),
+              ListTile(
+                onTap: () {},
+                title: const Text("Промокод"),
+                leading: Icon(SeverMetropol.Icon_Redeem_Card,
+                    color: Colors.red.shade900),
+              ),
+              ListTile(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const OrdersPage()));
+                },
+                title: const Text("История заказов"),
+                leading: Icon(SeverMetropol.Icon_History,
+                    color: Colors.red.shade900),
+              ),
+              ListTile(
+                onTap: () async {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const DeliveryAddressPage()));
+                },
+                title: const Text("Адреса доставки"),
+                leading:
+                    Icon(SeverMetropol.Icon_Place, color: Colors.red.shade900),
+              ),
+              const Divider(
+                color: Color(0xFFEFF3F4),
+                thickness: 2,
+                indent: 16,
+                endIndent: 16,
+              ),
+              ListTile(
+                onTap: () async {},
+                title: const Text("Смена пароля"),
+                leading:
+                    Icon(SeverMetropol.Icon_Lock, color: Colors.red.shade900),
+              ),
+              Mutation(
+                options: MutationOptions(
+                  document: gql(logoffClient),
+                  onCompleted: (result) {
+                    Provider.of<LoginState>(context, listen: false).loggedIn =
+                        false;
+                    context.push('/login');
+                  },
+                ),
+                builder: (runMutation, result) {
+                  return ListTile(
+                    onTap: () async {
+                      runMutation({});
+                    },
+                    title: const Text("Выход из приложения"),
+                    leading: Icon(SeverMetropol.Icon_Logout,
+                        color: Colors.red.shade900),
+                  );
+                },
+              ),
+            ],
+          );
+        });
   }
 }
