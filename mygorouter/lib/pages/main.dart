2aba493f266a5c8a3ce2ba1aae9caf51fb766e6d
Art Navsegda <artnavsegda@gmail.com>
2022-02-08 20:03:50 +0300

    Update main.dart

--- mygorouter/lib/pages/main.dart
+++ mygorouter/lib/pages/main.dart
@@ -8,49 +8,91 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Query(
-      options: QueryOptions(
-        document: gql(getTopBlocks),
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Query(
+              options: QueryOptions(
+                document: gql(getActions),
+              ),
+              builder: (result, {fetchMore, refetch}) {
+                print(result);
+                if (result.isLoading || result.hasException) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                List<GraphAction> actions = List<GraphAction>.from(result
+                    .data!['getActions']
+                    .map((model) => GraphAction.fromJson(model)));
+
+                return Column(
+                    children: actions
+                        .map(
+                          (e) => ListTile(
+                            title: Text(e.name),
+                            onTap: () {
+                              context.goNamed('product', params: {
+                                'tab': '0',
+                                'id': '${e.iD}',
+                              });
+                            },
+                          ),
+                        )
+                        .toList()
+                        .cast<Widget>());
+              },
+            ),
+            Query(
+              options: QueryOptions(
+                document: gql(getTopBlocks),
+              ),
+              builder: (result, {fetchMore, refetch}) {
+                print(result);
+                if (result.isLoading || result.hasException) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                List<GraphTopBlock> topBlocks = List<GraphTopBlock>.from(result
+                    .data!['getTopBlocks']
+                    .map((model) => GraphTopBlock.fromJson(model)));
+
+                return Column(
+                    children: topBlocks
+                        .map((block) {
+                          return Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(block.name),
+                              Column(
+                                  children: block.products
+                                      .map(
+                                        (e) => ListTile(
+                                          title: Text(e.name),
+                                          onTap: () {
+                                            context.goNamed('product', params: {
+                                              'tab': '0',
+                                              'id': '${e.iD}',
+                                            });
+                                          },
+                                        ),
+                                      )
+                                      .toList()
+                                      .cast<Widget>()),
+                            ],
+                          );
+                        })
+                        .toList()
+                        .cast<Widget>());
+              },
+            ),
+          ],
+        ),
       ),
-      builder: (result, {fetchMore, refetch}) {
-        print(result);
-        if (result.isLoading || result.hasException) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-
-        List<GraphTopBlock> topBlocks = List<GraphTopBlock>.from(result
-            .data!['getTopBlocks']
-            .map((model) => GraphTopBlock.fromJson(model)));
-
-        return ListView.builder(
-            itemCount: topBlocks.length,
-            itemBuilder: (context, index) {
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(topBlocks[index].name),
-                  Column(
-                      children: topBlocks[index]
-                          .products
-                          .map(
-                            (e) => ListTile(
-                              title: Text(e.name),
-                              onTap: () {
-                                context.goNamed('product', params: {
-                                  'tab': '0',
-                                  'id': '${e.iD}',
-                                });
-                              },
-                            ),
-                          )
-                          .toList()
-                          .cast<Widget>()),
-                ],
-              );
-            });
-      },
     );
   }
 }
