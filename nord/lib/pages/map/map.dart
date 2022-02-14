e92e3c8d2ffd060658669e7a28b6489cdc6802bb
Art Navsegda <artnavsegda@gmail.com>
2022-02-14 04:02:13 +0300

    Update map.dart

--- nord/lib/pages/map/map.dart
+++ nord/lib/pages/map/map.dart
@@ -10,76 +10,96 @@ class MapPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-/*     return Query(
+    return Query(
       options: QueryOptions(
-        document: gql(getActions),
+        document: gql(getShops),
       ),
-    ); */
+      builder: (result, {fetchMore, refetch}) {
+        if (result.hasException) {
+          return Scaffold(
+            appBar: AppBar(),
+            body: Center(
+                child: Text(
+              result.exception.toString(),
+            )),
+          );
+        }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Кондитерские и кафе'),
-        leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: Image.asset('assets/Icon-West.png')),
-      ),
-      body: Stack(
-        children: [
-          const GoogleMap(
-            initialCameraPosition: CameraPosition(
-              target: LatLng(37.42796133580664, -122.085749655962),
-              zoom: 14.4746,
-            ),
+        if (result.isLoading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        List<GraphShop> shops = List<GraphShop>.from(
+            result.data!['getShops'].map((model) => GraphShop.fromJson(model)));
+
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Кондитерские и кафе'),
+            leading: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Image.asset('assets/Icon-West.png')),
           ),
-          SizedBox.expand(
-            child: DraggableScrollableSheet(
-              builder:
-                  (BuildContext context, ScrollController scrollController) {
-                return Container(
-                  color: Colors.white,
-                  child: ListView(
-                    controller: scrollController,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: const TextField(
-                          decoration: InputDecoration(
-                              hintText: 'Поиск по названию или адресу',
-                              isDense: true,
-                              contentPadding: EdgeInsets.symmetric(
-                                  horizontal: 16, vertical: 8),
-                              border: OutlineInputBorder(
-                                borderSide: BorderSide.none,
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(2.0),
-                                ),
-                              ),
-                              filled: true),
-                        ),
+          body: Stack(
+            children: [
+              const GoogleMap(
+                initialCameraPosition: CameraPosition(
+                  target: LatLng(37.42796133580664, -122.085749655962),
+                  zoom: 14.4746,
+                ),
+              ),
+              SizedBox.expand(
+                child: DraggableScrollableSheet(
+                  builder: (BuildContext context,
+                      ScrollController scrollController) {
+                    return Container(
+                      color: Colors.white,
+                      child: ListView(
+                        controller: scrollController,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                  hintText: 'Поиск по названию или адресу',
+                                  isDense: true,
+                                  contentPadding: EdgeInsets.symmetric(
+                                      horizontal: 16, vertical: 8),
+                                  border: OutlineInputBorder(
+                                    borderSide: BorderSide.none,
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(2.0),
+                                    ),
+                                  ),
+                                  filled: true),
+                            ),
+                          ),
+                          ListTile(
+                            isThreeLine: true,
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const ShopPage()));
+                            },
+                            title: const Text('Невский, 6'),
+                            subtitle: const Text(
+                                'Сегодня открыто до 22:00\nАдмиралтейская'),
+                            trailing: Image.asset('assets/Icon-Direction.png'),
+                          ),
+                        ],
                       ),
-                      ListTile(
-                        isThreeLine: true,
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const ShopPage()));
-                        },
-                        title: const Text('Невский, 6'),
-                        subtitle: const Text(
-                            'Сегодня открыто до 22:00\nАдмиралтейская'),
-                        trailing: Image.asset('assets/Icon-Direction.png'),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
