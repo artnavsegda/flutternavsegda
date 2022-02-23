bf364a733f179cfe31002292b2ff5fa149dc6012
Art Navsegda <artnavsegda@gmail.com>
2022-02-23 16:37:38 +0300

    Update utils.dart

--- nord/lib/utils.dart
+++ nord/lib/utils.dart
@@ -23,3 +23,7 @@ MaterialColor createMaterialColor(Color color) {
 Color hexToColor(String code) {
   return Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
 }
+
+String periodDate(int dateStart, int dateEnd) {
+  return "";
+}
