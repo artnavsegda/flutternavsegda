f2f1ad60be607e9cc8e17b39ccae59a8f41c33ae
Art Navsegda <artnavsegda@gmail.com>
2022-02-25 18:59:23 +0300

    Update shop.dart

--- nord/lib/pages/shop/shop.dart
+++ nord/lib/pages/shop/shop.dart
@@ -10,168 +10,162 @@ class ShopPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Scaffold(
-          extendBodyBehindAppBar: true,
-          body: Stack(
-            children: [
-              FractionallySizedBox(
-                heightFactor: 0.55,
-                child: PageView(
-                  children: [
-                    Image.asset('assets/placeholder/shop.png',
-                        fit: BoxFit.cover),
-                    ...shop.pictures.map(
-                        (picture) => Image.network(picture, fit: BoxFit.cover))
-                  ],
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: Stack(
+        children: [
+          FractionallySizedBox(
+            heightFactor: 0.55,
+            child: PageView(
+              children: [
+                Image.asset('assets/placeholder/shop.png', fit: BoxFit.cover),
+                ...shop.pictures
+                    .map((picture) => Image.network(picture, fit: BoxFit.cover))
+              ],
+            ),
+          ),
+          Positioned(
+            top: 36,
+            left: 12,
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(32.0, 32.0),
+                  shape: const CircleBorder(),
+                  primary: Colors.white54,
+                  onPrimary: Colors.red,
                 ),
-              ),
-              Positioned(
-                top: 36,
-                left: 12,
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(32.0, 32.0),
-                      shape: const CircleBorder(),
-                      primary: Colors.white54,
-                      onPrimary: Colors.red,
-                    ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Icon(
-                      SeverMetropol.Icon_Close,
-                      color: Theme.of(context).colorScheme.primary,
-                    )),
-              ),
-              SizedBox.expand(
-                child: DraggableScrollableSheet(
-                    minChildSize: 0.5,
-                    maxChildSize: 0.9,
-                    builder: (context, scrollController) {
-                      return SafeArea(
-                        child: Container(
-                          color: Colors.white,
-                          child: ListView(
-                            controller: scrollController,
-                            children: [
-                              ListTile(
-                                isThreeLine: true,
-                                subtitle: Text(shop.address ??
-                                    '194358, Санкт-Петербург, проспект Просвещения, 19'),
-                                title: Text(shop.name),
-                              ),
-                              ListTile(
-                                subtitle: Text('Метро'),
-                                title: Wrap(
-                                  children: shop.metroStations
-                                      .map((metroStation) => Wrap(
-                                            crossAxisAlignment:
-                                                WrapCrossAlignment.center,
-                                            children: [
-                                              Text('⬤ ',
-                                                  style: TextStyle(
-                                                      fontSize: 7.0,
-                                                      color: hexToColor(
-                                                          metroStation
-                                                              .colorLine))),
-                                              Text(metroStation.stationName),
-                                              SizedBox(width: 10)
-                                            ],
-                                          ))
-                                      .toList()
-                                      .cast<Widget>(),
-                                ),
-                              ),
-                              Divider(),
-                              ListTile(
-                                subtitle: Text('Телефон'),
-                                title: Text('+7 (812) 611-09-27'),
-                              ),
-                              Divider(),
-                              ListTile(
-                                onTap: () {
-                                  showModalBottomSheet(
-                                    backgroundColor: Colors.white,
-                                    isScrollControlled: true,
-                                    context: context,
-                                    builder: (context) {
-                                      return Column(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Icon(
+                  SeverMetropol.Icon_Close,
+                  color: Theme.of(context).colorScheme.primary,
+                )),
+          ),
+          SizedBox.expand(
+            child: DraggableScrollableSheet(
+                minChildSize: 0.5,
+                maxChildSize: 0.9,
+                builder: (context, scrollController) {
+                  return SafeArea(
+                    child: Container(
+                      color: Colors.white,
+                      child: ListView(
+                        controller: scrollController,
+                        children: [
+                          ListTile(
+                            isThreeLine: true,
+                            subtitle: Text(shop.address ??
+                                '194358, Санкт-Петербург, проспект Просвещения, 19'),
+                            title: Text(shop.name),
+                          ),
+                          ListTile(
+                            subtitle: Text('Метро'),
+                            title: Wrap(
+                              children: shop.metroStations
+                                  .map((metroStation) => Wrap(
                                         crossAxisAlignment:
-                                            CrossAxisAlignment.stretch,
-                                        mainAxisSize: MainAxisSize.min,
+                                            WrapCrossAlignment.center,
                                         children: [
-                                          const ListTile(
-                                            title: Center(
-                                                child: Text(
-                                              'Режим работы',
-                                            )),
-                                          ),
-                                          Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceBetween,
-                                            children: const [
-                                              Text('Понедельник'),
-                                              Text('09:30–20:00'),
-                                            ],
-                                          ),
-                                          Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceBetween,
-                                            children: const [
-                                              Text('Вторник'),
-                                              Text('09:30–20:00'),
-                                            ],
-                                          ),
-                                          Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceBetween,
-                                            children: const [
-                                              Text('Среда'),
-                                              Text('09:30–20:00'),
-                                            ],
-                                          ),
-                                          Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceBetween,
-                                            children: const [
-                                              Text('Четверг'),
-                                              Text('09:30–20:00'),
-                                            ],
-                                          ),
+                                          Text('⬤ ',
+                                              style: TextStyle(
+                                                  fontSize: 7.0,
+                                                  color: hexToColor(
+                                                      metroStation.colorLine))),
+                                          Text(metroStation.stationName),
+                                          SizedBox(width: 10)
                                         ],
-                                      );
-                                    },
+                                      ))
+                                  .toList()
+                                  .cast<Widget>(),
+                            ),
+                          ),
+                          Divider(),
+                          ListTile(
+                            subtitle: Text('Телефон'),
+                            title: Text('+7 (812) 611-09-27'),
+                          ),
+                          Divider(),
+                          ListTile(
+                            onTap: () {
+                              showModalBottomSheet(
+                                backgroundColor: Colors.white,
+                                isScrollControlled: true,
+                                context: context,
+                                builder: (context) {
+                                  return Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.stretch,
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      const ListTile(
+                                        title: Center(
+                                            child: Text(
+                                          'Режим работы',
+                                        )),
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: const [
+                                          Text('Понедельник'),
+                                          Text('09:30–20:00'),
+                                        ],
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: const [
+                                          Text('Вторник'),
+                                          Text('09:30–20:00'),
+                                        ],
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: const [
+                                          Text('Среда'),
+                                          Text('09:30–20:00'),
+                                        ],
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: const [
+                                          Text('Четверг'),
+                                          Text('09:30–20:00'),
+                                        ],
+                                      ),
+                                    ],
                                   );
                                 },
-                                subtitle: const Text('Режим работы'),
-                                title: const Text('Завтра откроется в 10:00'),
-                                trailing: Icon(
-                                  SeverMetropol.Icon_Expand_More,
-                                  color: Theme.of(context).colorScheme.primary,
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: ElevatedButton(
-                                    onPressed: () {},
-                                    child: const Text('Построить маршрут')),
-                              ),
-                              TextButton(
-                                  onPressed: () {},
-                                  child: const Text(
-                                      'Заберу заказ в этом заведении')),
-                            ],
+                              );
+                            },
+                            subtitle: const Text('Режим работы'),
+                            title: const Text('Завтра откроется в 10:00'),
+                            trailing: Icon(
+                              SeverMetropol.Icon_Expand_More,
+                              color: Theme.of(context).colorScheme.primary,
+                            ),
                           ),
-                        ),
-                      );
-                    }),
-              ),
-            ],
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: ElevatedButton(
+                                onPressed: () {},
+                                child: const Text('Построить маршрут')),
+                          ),
+                          TextButton(
+                              onPressed: () {},
+                              child:
+                                  const Text('Заберу заказ в этом заведении')),
+                        ],
+                      ),
+                    ),
+                  );
+                }),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
