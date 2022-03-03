3154060a270bad1c6813ffa5b93fc94ee510d32c
Art Navsegda <artnavsegda@gmail.com>
2022-03-03 11:29:05 +0300

    Update gift_bonus.dart

--- nord/lib/pages/user/gift_bonus.dart
+++ nord/lib/pages/user/gift_bonus.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:nord/components/components.dart';
 import 'package:nord/sever_metropol_icons.dart';
 
@@ -9,6 +10,10 @@ class GiftBonusModalSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var maskFormatter = MaskTextInputFormatter(
+        mask: '+# (###) ###-##-##',
+        filter: {"#": RegExp(r'[0-9]')},
+        type: MaskAutoCompletionType.lazy);
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       mainAxisSize: MainAxisSize.min,
@@ -32,9 +37,11 @@ class GiftBonusModalSheet extends StatelessWidget {
               SliderCombo(),
               SizedBox(height: 12),
               TextField(
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [maskFormatter],
                   decoration: InputDecoration(
-                labelText: 'Номер телефона получателя',
-              )),
+                    labelText: 'Номер телефона получателя',
+                  )),
               SizedBox(height: 16),
               ElevatedButton(
                   onPressed: () {}, child: const Text('Подарить бонусы')),
