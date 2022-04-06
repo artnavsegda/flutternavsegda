b25b1018dcff7e88cb2a0dcfa9df743da90a55cb
Art Navsegda <artnavsegda@gmail.com>
2022-04-07 01:05:36 +0300

    Update discount_card.dart

--- nord/lib/pages/home/discount_card.dart
+++ nord/lib/pages/home/discount_card.dart
@@ -106,104 +106,98 @@ class CardLoggedIn extends StatelessWidget {
         aspectRatio: 16 / 9,
         child: Material(
           color: Colors.transparent,
-          child: InkWell(
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const QrPage()));
-            },
-            child: Stack(
-              children: [
-                Positioned(
-                  left: 16,
-                  top: 16,
-                  child: Image.asset('assets/Logo-Blue.png'),
-                ),
-                Positioned(
-                  left: 16,
-                  top: 40,
-                  child: Image.asset('assets/Logo-Red.png'),
-                ),
+          child: Stack(
+            children: [
+              Positioned(
+                left: 16,
+                top: 16,
+                child: Image.asset('assets/Logo-Blue.png'),
+              ),
+              Positioned(
+                left: 16,
+                top: 40,
+                child: Image.asset('assets/Logo-Red.png'),
+              ),
 /*                 Positioned(
-                  right: 16,
-                  top: 16,
-                  child: Image.asset('assets/Special-Icon-QR-Code-Scanner.png'),
+                right: 16,
+                top: 16,
+                child: Image.asset('assets/Special-Icon-QR-Code-Scanner.png'),
+              ),
+              Positioned(
+                top: 88,
+                right: 43,
+                child: Transform.rotate(
+                    angle: pi, child: Image.asset('assets/Union.png')),
+              ),
+              const Positioned(
+                top: 113,
+                right: 97,
+                child: Text(
+                  'Ваша карта',
+                  style: TextStyle(color: Colors.white),
                 ),
-                Positioned(
-                  top: 88,
-                  right: 43,
-                  child: Transform.rotate(
-                      angle: pi, child: Image.asset('assets/Union.png')),
+              ), */
+              const Positioned(
+                bottom: 64,
+                left: 16,
+                child: Text(
+                  'У вас',
+                  style: TextStyle(color: Colors.white),
                 ),
-                const Positioned(
-                  top: 113,
-                  right: 97,
-                  child: Text(
-                    'Ваша карта',
-                    style: TextStyle(color: Colors.white),
+              ),
+              Positioned(
+                bottom: 34,
+                left: 16,
+                child: Text(
+                  '120 бонусов',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineSmall!
+                      .copyWith(color: Colors.white),
+                ),
+              ),
+              Positioned(
+                bottom: 16,
+                left: 16,
+                child: Text(
+                  '5% начисление',
+                  style: TextStyle(color: Color(0xB2FFFFFF), fontSize: 12),
+                ),
+              ),
+              Positioned(
+                bottom: 12,
+                right: 16,
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: Colors.white),
+                    primary: Colors.white,
                   ),
-                ), */
-                const Positioned(
-                  bottom: 64,
-                  left: 16,
-                  child: Text(
-                    'У вас',
-                    style: TextStyle(color: Colors.white),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const QrPage()));
+                  },
+                  child: Text('QR-код'),
+                ),
+              ),
+              Positioned(
+                top: 4,
+                right: 4,
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LoyaltyPage()));
+                  },
+                  icon: Icon(
+                    SeverMetropol.Icon_Info,
+                    color: Colors.white,
                   ),
                 ),
-                Positioned(
-                  bottom: 34,
-                  left: 16,
-                  child: Text(
-                    '120 бонусов',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headlineSmall!
-                        .copyWith(color: Colors.white),
-                  ),
-                ),
-                Positioned(
-                  bottom: 16,
-                  left: 16,
-                  child: Text(
-                    '5% начисление',
-                    style: TextStyle(color: Color(0xB2FFFFFF), fontSize: 12),
-                  ),
-                ),
-                Positioned(
-                  bottom: 12,
-                  right: 16,
-                  child: OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                      side: BorderSide(color: Colors.white),
-                      primary: Colors.white,
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const QrPage()));
-                    },
-                    child: Text('QR-код'),
-                  ),
-                ),
-                Positioned(
-                  top: 4,
-                  right: 4,
-                  child: IconButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const LoyaltyPage()));
-                    },
-                    icon: Icon(
-                      SeverMetropol.Icon_Info,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
