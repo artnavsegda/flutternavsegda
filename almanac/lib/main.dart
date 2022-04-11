83a12590388cb05308145eaecc8070d0ce4dd49c
Art Navsegda <artnavsegda@gmail.com>
2022-04-11 11:14:42 +0300

    Update main.dart

--- almanac/lib/main.dart
+++ almanac/lib/main.dart
@@ -15,54 +15,63 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.indigo,
       ),
-      home: Scaffold(
-          appBar: AppBar(
-            leadingWidth: 200.0,
-            leading: TextButton(
-              child: Text(
-                'Choose',
-                style: TextStyle(color: Colors.white),
-              ),
-              onPressed: () {},
-            ),
-          ),
-          body: Row(
-            children: [
-              SizedBox(
-                width: 300,
-                child: FutureBuilder<List<FileSystemEntity>>(
-                  future: Directory('/').list().toList(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return ListView(
-                        children: [
-                          ...snapshot.data!.map((e) => Text(e.path)),
-                        ],
-                      );
-                    } else
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                  },
-                ),
-              ),
-              Expanded(
-                child: SfPdfViewer.file(
-                    File("C:\\EFF_charts_2202\\BIKF_KEFLAVIK\\APP\\H01.pdf")),
-              ),
-            ],
-          )),
+      home: MainPage(),
     );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leadingWidth: 200.0,
+          leading: TextButton(
+            child: Text(
+              'Choose',
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () => showDialog(
+                context: context,
+                builder: (context) => Dialog(
+                      child: Container(
+                        width: 300,
+                        height: 300,
+                      ),
+                    )),
+          ),
+        ),
+        body: Row(
+          children: [
+            SizedBox(
+              width: 300,
+              child: FutureBuilder<List<FileSystemEntity>>(
+                future: Directory('/').list().toList(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView(
+                      children: [
+                        ...snapshot.data!.map((e) => Text(e.path)),
+                      ],
+                    );
+                  } else
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                },
+              ),
+            ),
+            Expanded(
+              child: SfPdfViewer.file(
+                  File("C:\\EFF_charts_2202\\BIKF_KEFLAVIK\\APP\\H01.pdf")),
+            ),
+          ],
+        ));
   }
 }
