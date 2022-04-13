fa6a764a62d9e3da8e5e76f37249a992f1ef264e
Art Navsegda <artnavsegda@gmail.com>
2022-04-13 15:14:17 +0300

    Update main.dart

--- almanac/lib/main.dart
+++ almanac/lib/main.dart
@@ -3,8 +3,11 @@ import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
   runApp(const MyApp());
 }
 
