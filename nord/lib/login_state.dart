d69592501facc861d1bcb0f313d3dedab461d2ef
Art Navsegda <artnavsegda@gmail.com>
2022-04-23 14:13:14 +0300

    Update login_state.dart

--- nord/lib/login_state.dart
+++ nord/lib/login_state.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'gql.dart';
 
 class LoginState extends ChangeNotifier {
   final SharedPreferences prefs;
@@ -43,6 +45,37 @@ class CartState with ChangeNotifier {
   set cartAmount(int newAmount) {
     _cartAmount = newAmount;
     prefs.setInt('cart', newAmount);
+    notifyListeners();
+  }
+}
+
+class CartModel with ChangeNotifier {
+  final SharedPreferences prefs;
+  List<GraphCartRow> _cart = [];
+
+  CartModel(this.prefs) {
+    _cart = (jsonDecode(prefs.getString('cartcontent') ?? "[]") as List)
+        .map((model) => GraphCartRow.fromJson(model))
+        .toList();
+  }
+
+  List<GraphCartRow> get cart => _cart;
+  set cart(List<GraphCartRow> value) {
+    _cart = value;
+    prefs.setString('cartcontent', jsonEncode(_cart));
+    notifyListeners();
+  }
+
+  addToCart({required int id}) {
+    if (cart.any((element) => element.productID == id)) return;
+    cart.add(GraphCartRow(
+      productID: id,
+      rowID: 1,
+      characteristics: [],
+      quantity: 1,
+      productName: 'test',
+      amount: 100,
+    ));
     notifyListeners();
   }
 }
