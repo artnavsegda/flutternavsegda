7f8a5bd63e91a2cc175e2bcccf0c17ae4abdb69a
Art Navsegda <artnavsegda@gmail.com>
2022-04-26 15:42:39 +0300

    s

--- almanac/macos/Flutter/GeneratedPluginRegistrant.swift
+++ almanac/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus_macos
+import pdfx
+import shared_preferences_macos
 import syncfusion_pdfviewer_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  PdfxPlugin.register(with: registry.registrar(forPlugin: "PdfxPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SyncfusionFlutterPdfViewerPlugin.register(with: registry.registrar(forPlugin: "SyncfusionFlutterPdfViewerPlugin"))
 }
