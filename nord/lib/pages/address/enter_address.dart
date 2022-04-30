032081c379da286aa8f2149da56221390e8b484c
Art Navsegda <artnavsegda@gmail.com>
2022-05-01 02:16:26 +0300

    Update enter_address.dart

--- nord/lib/pages/address/enter_address.dart
+++ nord/lib/pages/address/enter_address.dart
@@ -8,12 +8,19 @@ class EnterAddress extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Введите адрес')),
-      body: GoogleMap(
-        myLocationEnabled: true,
-        initialCameraPosition: CameraPosition(
-          target: LatLng(37.42796133580664, -122.085749655962),
-          zoom: 14.4746,
-        ),
+      body: Column(
+        children: [
+          ElevatedButton(onPressed: () {}, child: Text('Сохранить')),
+          TextField(),
+          TextField(),
+          GoogleMap(
+            myLocationEnabled: true,
+            initialCameraPosition: CameraPosition(
+              target: LatLng(37.42796133580664, -122.085749655962),
+              zoom: 14.4746,
+            ),
+          ),
+        ],
       ),
     );
   }
