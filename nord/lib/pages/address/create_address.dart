b0906cad31d7f3c7589cd49942442d14db568dbe
Art Navsegda <artnavsegda@gmail.com>
2022-05-05 10:36:27 +0300

    Update create_address.dart

--- nord/lib/pages/address/create_address.dart
+++ nord/lib/pages/address/create_address.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nord/gql.dart';
+import 'package:nord/utils.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 class CreateAddress extends StatelessWidget {
@@ -32,12 +33,39 @@ class CreateAddress extends StatelessWidget {
         ],
       ),
       bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ElevatedButton(
-          onPressed: () {},
-          child: Text('Сохранить адрес'),
-        ),
-      ),
+          padding: const EdgeInsets.all(16.0),
+          child: Mutation(
+              options: MutationOptions(
+                document: gql(promocodeActivation),
+                onError: (error) {
+                  showErrorAlert(context, error!.graphqlErrors[0].message);
+                },
+                onCompleted: (resultData) {
+                  print(resultData);
+                  if (resultData != null) {
+                    GraphPromocodeResult nordPromocodeResult =
+                        GraphPromocodeResult.fromJson(
+                            resultData['promocodeActivation']);
+                    if (nordPromocodeResult.result == 0) {
+                      Navigator.pop(context);
+                      if (nordPromocodeResult.productID != null) {}
+                    }
+                    if (nordPromocodeResult.errorMessage?.isNotEmpty ?? false)
+                      showErrorAlert(
+                          context, nordPromocodeResult.errorMessage ?? '');
+                  }
+                },
+              ),
+              builder: (runMutation, result) {
+                return ElevatedButton(
+                  child: const Text('Сохранить адрес'),
+                  onPressed: () {
+                    runMutation({
+                      'promoCode': textController.text,
+                    });
+                  },
+                );
+              })),
     );
   }
 }
