3e53be94139301e3e83c5a1121ec918528abce39
Art Navsegda <artnavsegda@gmail.com>
2022-05-08 18:15:20 +0300

    Update address.dart

--- nord/lib/pages/address/address.dart
+++ nord/lib/pages/address/address.dart
@@ -29,6 +29,15 @@ class _AddressPageState extends State<AddressPage> {
             )),
         title: const Text('Адрес доставки или кафе'),
       ),
+      body: Stack(children: [
+        GoogleMap(
+          myLocationEnabled: true,
+          initialCameraPosition: CameraPosition(
+            target: LatLng(37.42796133580664, -122.085749655962),
+            zoom: 14.4746,
+          ),
+        ),
+      ]),
     );
   }
 }
