f73a02044a908d5a1a089af04b4f3a9bbf4d084b
Art Navsegda <artnavsegda@gmail.com>
2022-05-26 22:02:37 +0300

    Update map.dart

--- nord/lib/pages/map/map.dart
+++ nord/lib/pages/map/map.dart
@@ -28,141 +28,154 @@ class MapPage extends StatelessWidget {
               color: Theme.of(context).colorScheme.primary,
             )),
       ),
-      body: Query(
-        options: QueryOptions(
-          document: gql(getShops),
-        ),
-        builder: (result, {fetchMore, refetch}) {
-          if (result.hasException) {
-            return ErrorPage(
-              reload: () {
-                refetch!();
-              },
-              errorText: result.exception?.graphqlErrors[0].message ?? '',
-            );
-          }
-
-          if (result.isLoading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          List<GraphShop> shops = List<GraphShop>.from(result.data!['getShops']
-              .map((model) => GraphShop.fromJson(model)));
-
-          return FutureBuilder<Position>(
-            future: Geolocator.getCurrentPosition(),
-            builder: (context, snapshot) {
-              LatLng myLocation = LatLng(59.9311, 30.3609);
-              if (snapshot.hasData) {
-                myLocation =
-                    LatLng(snapshot.data!.latitude, snapshot.data!.longitude);
-                shops.sort((a, b) => Geolocator.distanceBetween(
-                        a.latitude ?? 0,
-                        a.longitude ?? 0,
-                        myLocation.latitude,
-                        myLocation.longitude)
-                    .compareTo(Geolocator.distanceBetween(
-                        b.latitude ?? 0,
-                        b.longitude ?? 0,
-                        myLocation.latitude,
-                        myLocation.longitude)));
-              }
-
-              return Stack(
-                children: [
-                  GoogleMap(
-                    myLocationEnabled: true,
-                    initialCameraPosition: CameraPosition(
-                      target: myLocation,
-                      zoom: 10,
-                    ),
-                    markers: shops.map(
-                      (shop) {
-                        return Marker(
-                            onTap: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          ShopPage(shop: shop)));
-                            },
-                            markerId: MarkerId(shop.iD.toString()),
-                            position: LatLng(
-                                shop.latitude ?? 0, shop.longitude ?? 0));
-                      },
-                    ).toSet(),
-                  ),
-                  DraggableScrollableSheet(
-                    minChildSize: 0.15,
-                    builder: (context, scrollController) {
-                      return Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(4.0),
-                        ),
-                        child: StatefulBuilder(builder: (context, setState) {
-                          return ListView(
-                            controller: scrollController,
-                            children: [
-                              DragHandle(),
-                              Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: TextField(
-                                  onChanged: (value) {
-                                    setState(() => textFilter = value);
-                                  },
-                                  decoration: InputDecoration(
-                                      hintText: 'Поиск по названию или адресу',
-                                      isDense: true,
-                                      contentPadding: EdgeInsets.symmetric(
-                                          horizontal: 16, vertical: 8),
-                                      border: OutlineInputBorder(
-                                        borderSide: BorderSide.none,
-                                        borderRadius: BorderRadius.all(
-                                          Radius.circular(2.0),
-                                        ),
-                                      ),
-                                      filled: true),
-                                ),
-                              ),
-                              ...shops
-                                  .map((shop) => ShopTile(
-                                        shop: shop,
-                                        onTap: () {
-                                          if (onSelect == null) {
-                                            Navigator.push(
-                                                context,
-                                                MaterialPageRoute(
-                                                    builder: (context) =>
-                                                        ShopPage(shop: shop)));
-                                          } else {
-                                            Navigator.pop(context);
-                                            onSelect!(shop);
-                                          }
-                                        },
-                                      ))
-                                  .where((element) {
-                                return element.shop.address!
-                                        .toLowerCase()
-                                        .contains(textFilter) ||
-                                    element.shop.name
-                                        .toLowerCase()
-                                        .contains(textFilter);
-                              })
-                            ],
-                          );
-                        }),
-                      );
+      body: FutureBuilder<BitmapDescriptor>(
+          future: BitmapDescriptor.fromAssetImage(
+              ImageConfiguration(), 'assets/3.0x/Pin.png'),
+          builder: (context, assetSnapshot) {
+            return Query(
+              options: QueryOptions(
+                document: gql(getShops),
+              ),
+              builder: (result, {fetchMore, refetch}) {
+                if (result.hasException) {
+                  return ErrorPage(
+                    reload: () {
+                      refetch!();
                     },
-                  ),
-                ],
-              );
-            },
-          );
-        },
-      ),
+                    errorText: result.exception?.graphqlErrors[0].message ?? '',
+                  );
+                }
+
+                if (result.isLoading) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                List<GraphShop> shops = List<GraphShop>.from(result
+                    .data!['getShops']
+                    .map((model) => GraphShop.fromJson(model)));
+
+                return FutureBuilder<Position>(
+                  future: Geolocator.getCurrentPosition(),
+                  builder: (context, snapshot) {
+                    LatLng myLocation = LatLng(59.9311, 30.3609);
+                    if (snapshot.hasData) {
+                      myLocation = LatLng(
+                          snapshot.data!.latitude, snapshot.data!.longitude);
+                      shops.sort((a, b) => Geolocator.distanceBetween(
+                              a.latitude ?? 0,
+                              a.longitude ?? 0,
+                              myLocation.latitude,
+                              myLocation.longitude)
+                          .compareTo(Geolocator.distanceBetween(
+                              b.latitude ?? 0,
+                              b.longitude ?? 0,
+                              myLocation.latitude,
+                              myLocation.longitude)));
+                    }
+
+                    return Stack(
+                      children: [
+                        GoogleMap(
+                          myLocationEnabled: true,
+                          initialCameraPosition: CameraPosition(
+                            target: myLocation,
+                            zoom: 10,
+                          ),
+                          markers: shops.map(
+                            (shop) {
+                              return Marker(
+                                  icon: assetSnapshot.data ??
+                                      BitmapDescriptor.defaultMarker,
+                                  onTap: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                ShopPage(shop: shop)));
+                                  },
+                                  markerId: MarkerId(shop.iD.toString()),
+                                  position: LatLng(
+                                      shop.latitude ?? 0, shop.longitude ?? 0));
+                            },
+                          ).toSet(),
+                        ),
+                        DraggableScrollableSheet(
+                          minChildSize: 0.15,
+                          builder: (context, scrollController) {
+                            return Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(4.0),
+                              ),
+                              child:
+                                  StatefulBuilder(builder: (context, setState) {
+                                return ListView(
+                                  controller: scrollController,
+                                  children: [
+                                    DragHandle(),
+                                    Padding(
+                                      padding: const EdgeInsets.all(16.0),
+                                      child: TextField(
+                                        onChanged: (value) {
+                                          setState(() => textFilter = value);
+                                        },
+                                        decoration: InputDecoration(
+                                            hintText:
+                                                'Поиск по названию или адресу',
+                                            isDense: true,
+                                            contentPadding:
+                                                EdgeInsets.symmetric(
+                                                    horizontal: 16,
+                                                    vertical: 8),
+                                            border: OutlineInputBorder(
+                                              borderSide: BorderSide.none,
+                                              borderRadius: BorderRadius.all(
+                                                Radius.circular(2.0),
+                                              ),
+                                            ),
+                                            filled: true),
+                                      ),
+                                    ),
+                                    ...shops
+                                        .map((shop) => ShopTile(
+                                              shop: shop,
+                                              onTap: () {
+                                                if (onSelect == null) {
+                                                  Navigator.push(
+                                                      context,
+                                                      MaterialPageRoute(
+                                                          builder: (context) =>
+                                                              ShopPage(
+                                                                  shop: shop)));
+                                                } else {
+                                                  Navigator.pop(context);
+                                                  onSelect!(shop);
+                                                }
+                                              },
+                                            ))
+                                        .where((element) {
+                                      return element.shop.address!
+                                              .toLowerCase()
+                                              .contains(textFilter) ||
+                                          element.shop.name
+                                              .toLowerCase()
+                                              .contains(textFilter);
+                                    })
+                                  ],
+                                );
+                              }),
+                            );
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            );
+          }),
     );
   }
 }
