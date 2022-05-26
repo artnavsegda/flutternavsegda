cc63dc08b662ed648f9cf8570e8fc98e221f799d
Art Navsegda <artnavsegda@gmail.com>
2022-05-26 19:59:11 +0300

    Update success.dart

--- nord/lib/pages/shopping/success.dart
+++ nord/lib/pages/shopping/success.dart
@@ -7,14 +7,35 @@ class SuccessPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: ElevatedButton(
-        child: Text('OK'),
-        onPressed: () {
-          //Navigator.popUntil(context, ModalRoute.withName("/main"));
-          context.go('/main');
-        },
-      ),
+        body: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(
+          'assets/Illustration-Successful-Express-Order.png',
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            'Ваш заказ оформлен.\nУже собираем и упаковываем!',
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+              'Статус вашего заказа вы можете отследить, перейдя к списку ваших покупок в личном кабинете'),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ElevatedButton(
+            child: Text('Вернуться на главную'),
+            onPressed: () {
+              //Navigator.popUntil(context, ModalRoute.withName("/main"));
+              context.go('/main');
+            },
+          ),
+        ),
+      ],
     ));
   }
 }
