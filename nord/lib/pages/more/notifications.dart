510a6a01f67c0738a4fcde36f549141bfe06dd69
Art Navsegda <artnavsegda@gmail.com>
2022-05-31 01:22:07 +0300

    Update notifications.dart

--- nord/lib/pages/more/notifications.dart
+++ nord/lib/pages/more/notifications.dart
@@ -5,6 +5,8 @@ class NotificationsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text('Уведомления')),
+    );
   }
 }
