9a04a82edd7df6ede9fcf3c388e04463130f4e55
Art Navsegda <artnavsegda@gmail.com>
2022-05-30 23:21:41 +0300

    Update invite.dart

--- nord/lib/pages/user/invite.dart
+++ nord/lib/pages/user/invite.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:nord/sever_metropol_icons.dart';
 //import 'package:share_plus/share_plus.dart';
 
 class InvitePage extends StatelessWidget {
@@ -12,25 +13,42 @@ class InvitePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: MediaQuery.of(context).viewInsets,
-      child: Container(
-        padding: const EdgeInsets.all(15.0),
-        child: Wrap(
-          children: [
-            Center(
-              child: Column(
-                children: [
-                  Text(codeInviteFriend),
-                  TextButton(
-                      onPressed: () {
-                        Clipboard.setData(
-                            ClipboardData(text: codeInviteFriend));
-                      },
-                      child: const Text("Скопировать")),
-                ],
-              ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ListTile(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            title: Center(
+                child: Text(
+              '50 бонусов Вам и каждому другу!',
+              style: TextStyle(color: Color(0xFF56626C)),
+            )),
+            trailing: Icon(
+              SeverMetropol.Icon_Close,
+              color: Colors.red[900],
             ),
-            ElevatedButton(
-              child: const Text("ПОДЕЛИТСЯ КОДОМ"),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+            child: Text(
+                'Отправьте свой уникальный промокод друзьям, у которых ещё нет приложения «Север-Метрополь».\nПосле первой покупки друга на Ваш и его счёт поступит по 50 бонусов. Приглашайте друзей, получайте бонусы и делайте выгодные покупки!'),
+          ),
+          ListTile(
+            onTap: () {
+              Clipboard.setData(ClipboardData(text: codeInviteFriend));
+            },
+            title: Text(codeInviteFriend),
+            trailing: Icon(
+              SeverMetropol.Icon_Contact_Support,
+              color: Colors.red[900],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              child: const Text("Поделиться кодом"),
               onPressed: () {
                 //Share.share(codeInviteFriend);
               },
@@ -38,8 +56,8 @@ class InvitePage extends StatelessWidget {
                 minimumSize: const Size(double.infinity, 48),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
