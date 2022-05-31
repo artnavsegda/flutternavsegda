ed346d8a3cb4b072afbd46b0bc5616d67d1d7973
Art Navsegda <artnavsegda@gmail.com>
2022-05-31 11:15:07 +0300

    Update notifications.dart

--- nord/lib/pages/more/notifications.dart
+++ nord/lib/pages/more/notifications.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:nord/pages/error/error.dart';
 import 'package:nord/gql.dart';
 
 class NotificationsPage extends StatelessWidget {
@@ -15,7 +16,34 @@ class NotificationsPage extends StatelessWidget {
           document: gql(getNotifications),
         ),
         builder: (result, {fetchMore, refetch}) {
-          return ListView();
+          if (result.isLoading && result.data == null) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (result.hasException) {
+            return ErrorPage(
+                reload: () {
+                  refetch!();
+                },
+                errorText: result.exception.toString());
+            return SingleChildScrollView(
+              child: Text(result.exception.toString()),
+            );
+          }
+
+          List<GraphNotification> notifications = List<GraphNotification>.from(
+              result.data!['getNotifications']
+                  .map((model) => GraphNotification.fromJson(model)));
+          return ListView(
+            children: [
+              ...notifications.map((notification) => ListTile(
+                    title: Text(notification.caption ?? 'wtf'),
+                    subtitle: Text(notification.text ?? 'wtf'),
+                  ))
+            ],
+          );
         },
       ),
     );
