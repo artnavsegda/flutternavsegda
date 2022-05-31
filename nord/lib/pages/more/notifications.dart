4cfa72459c11a9ed51ee20ad61aeafa493862576
Art Navsegda <artnavsegda@gmail.com>
2022-05-31 04:52:17 +0300

    Update notifications.dart

--- nord/lib/pages/more/notifications.dart
+++ nord/lib/pages/more/notifications.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:nord/gql.dart';
 
 class NotificationsPage extends StatelessWidget {
   const NotificationsPage({Key? key}) : super(key: key);
@@ -7,6 +9,15 @@ class NotificationsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Уведомления')),
+      body: Query(
+        options: QueryOptions(
+          fetchPolicy: FetchPolicy.networkOnly,
+          document: gql(getNotifications),
+        ),
+        builder: (result, {fetchMore, refetch}) {
+          return ListView();
+        },
+      ),
     );
   }
 }
