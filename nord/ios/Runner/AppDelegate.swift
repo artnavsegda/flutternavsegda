2338ad2aa678aea06e275ffb221b4a0fdd2d0de7
Art Navsegda <artnavsegda@gmail.com>
2022-06-02 04:11:24 +0300

    yandex maps

--- nord/ios/Runner/AppDelegate.swift
+++ nord/ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import YandexMapsMobile
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,6 +10,7 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyArLAGgB0ve7ZZ3yxdbNyHVGl0HCq16kFQ")
+    YMKMapKit.setApiKey("5c7d7c5c-4960-4a49-9664-091d0215e80c")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
