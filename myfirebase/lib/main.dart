8723dd15c0bc978f7ae6955cea4199190dde39f2
Art Navsegda <artnavsegda@gmail.com>
2022-10-24 14:51:56 +0300

    Update main.dart

--- myfirebase/lib/main.dart
+++ myfirebase/lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
