71701be2fbbde8f8ed5416fd141540e63d872d52
Art Navsegda <artnavsegda@gmail.com>
2026-01-02 04:53:19 +0300

    Update main.dart

--- bgdemo/lib/main.dart
+++ bgdemo/lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:workmanager/workmanager.dart';
 
+@pragma('vm:entry-point')
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) async {
+    print("Background task: $task");
+    // Your background work here
+    return Future.value(true);
+  });
+}
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Workmanager().initialize(callbackDispatcher);
+  Workmanager().registerOneOffTask("task-id", "simpleTask");
   runApp(const MyApp());
 }
 
